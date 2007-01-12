9dfd8bc3021b592f0f795c31bdc1fc9124f318e0
sjfink <sjfink@f5eafffb-2e1d-0410-98e4-8ec43c5233c4>
2007-01-12 14:49:25 +0000

    initial check-in of Internal annotation
    git-svn-id: https://wala.svn.sourceforge.net/svnroot/wala/trunk@563 f5eafffb-2e1d-0410-98e4-8ec43c5233c4

---
+++ com.ibm.wala.shrike/src/com/ibm/wala/annotations/Internal.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2006 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package com.ibm.wala.annotations;
+
+/**
+ * An annotation which indicates a method which although
+ * public, should be considered an "internal" method, not exposed
+ * to the outside world or an adversary.
+ * 
+ * This annotation is placed in the shrike project just so it's
+ * at the top of the WALA project dependency hierarchy.  It doesn't have
+ * anything to do with shrike.
+ * 
+ * @author sjfink
+ *
+ */
+public @interface Internal {
+
+}
