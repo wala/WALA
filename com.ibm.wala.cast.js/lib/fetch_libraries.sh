6bb77209d9df09fbc6714e2a1e8aaf4359de6c6d
dolby-oss <dolby-oss@f5eafffb-2e1d-0410-98e4-8ec43c5233c4>
2007-02-08 14:26:40 +0000

    add script to fetch libraries js.jar and xalan.jar
    git-svn-id: https://wala.svn.sourceforge.net/svnroot/wala/trunk@661 f5eafffb-2e1d-0410-98e4-8ec43c5233c4

---
+++ com.ibm.wala.cast.js/lib/fetch_libraries.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+REAL_NAME=`realpath $0`
+MY_DIR=`dirname $REAL_NAME`
+
+TMP=$MY_DIR/tmp
+
+if [[ ! -e $MY_DIR/js.jar ]]; then
+  mkdir -p $TMP
+
+  cd $TMP
+
+  wget -O rhino1_6R5.zip ftp://ftp.mozilla.org/pub/mozilla.org/js/rhino1_6R5.zip 
+ 
+  unzip rhino1_6R5.zip rhino1_6R5/js.jar
+
+  cp rhino1_6R5/js.jar $MY_DIR/js.jar
+
+  cd $MY_DIR
+  rm -rf $TMP
+fi
+
+if [[ ! -e $MY_DIR/xalan.jar ]]; then
+  mkdir -p $TMP
+
+  cd $TMP
+
+  wget -O xalan-j_2_7_0-bin.tar.gz http://mirror.candidhosting.com/pub/apache/xml/xalan-j/xalan-j_2_7_0-bin.tar.gz
+
+  tar xzf xalan-j_2_7_0-bin.tar.gz xalan-j_2_7_0/xalan.jar
+
+  cp xalan-j_2_7_0/xalan.jar $MY_DIR/xalan.jar
+
+  cd $MY_DIR
+  rm -rf $TMP
+fi
