6f7e4c5b7de24e650f0d62eb52d5286ee4aa2d3e
tabee <tabee@f5eafffb-2e1d-0410-98e4-8ec43c5233c4>
2007-02-27 14:44:42 +0000

    2007 02/27, Naoshi Tabuch: tabee@jp.ibm.com Fix for Visual C++ + uses _alloca()/memset() instead of var-length arrays/bzero() + includes <malloc.h> for _alloca() # neither do not affect gcc compilation
    git-svn-id: https://wala.svn.sourceforge.net/svnroot/wala/trunk@797 f5eafffb-2e1d-0410-98e4-8ec43c5233c4

--- com.ibm.wala.cast/source/c/jni/Exceptions.cpp
+++ com.ibm.wala.cast/source/c/jni/Exceptions.cpp
@@ -3,6 +3,10 @@
 #include <string.h>
 #include "Exceptions.h"
 
+#ifdef _MSC_VER
+#include <malloc.h>
+#endif
+
 Exceptions::Exceptions(JNIEnv *java_env, jmp_buf& c_env) : 
   _java_env(java_env), 
   _c_env(c_env) 
@@ -23,8 +27,14 @@ void Exceptions::throwException(char *file_name, int line_number) {
   jthrowable real_ex = _java_env->ExceptionOccurred();
   _java_env->ExceptionClear();
 
+#ifdef _MSC_VER
+  int msglen = strlen(file_name) + 1024;
+  char* msg = (char*)_alloca(msglen);
+  memset(msg, msglen, 0);
+#else
   char msg[strlen(file_name) + 1024];
   bzero(msg, strlen(file_name) + 1024);
+#endif
   sprintf(msg, "exception at %s:%d", file_name, line_number);
   jstring java_message = _java_env->NewStringUTF(msg);
 
@@ -43,8 +53,14 @@ void Exceptions::throwException(char *file_name, int line_number) {
 
 void 
 Exceptions::throwException(char *file_name, int line_number, char *c_message) {
+#ifdef _MSC_VER
+  int msglen = strlen(file_name) + strlen(c_message) + 1024;
+  char* msg = (char*)_alloca(msglen);
+  memset(msg, msglen, 0);
+#else
   char msg[strlen(file_name) + strlen(c_message) + 1024]; 
   bzero(msg, strlen(file_name) + strlen(c_message) + 1024);
+#endif
   sprintf(msg, "exception at %s:%d: %s", file_name, line_number, c_message);
   jstring java_message = _java_env->NewStringUTF(msg);
   jthrowable ex = (jthrowable)_java_env->NewObject(_jre, _ctr, java_message);
