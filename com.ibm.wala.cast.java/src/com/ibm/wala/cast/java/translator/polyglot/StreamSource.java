8ee55f98f18faec399843853c79631bdade377a3
sjfink <sjfink@f5eafffb-2e1d-0410-98e4-8ec43c5233c4>
2007-03-12 16:09:53 +0000

    minor cleanups
    git-svn-id: https://wala.svn.sourceforge.net/svnroot/wala/trunk@850 f5eafffb-2e1d-0410-98e4-8ec43c5233c4

--- com.ibm.wala.cast.java/src/com/ibm/wala/cast/java/translator/polyglot/StreamSource.java
+++ com.ibm.wala.cast.java/src/com/ibm/wala/cast/java/translator/polyglot/StreamSource.java
@@ -16,7 +16,6 @@ package com.ibm.wala.cast.java.translator.polyglot;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.Reader;
 
 import polyglot.frontend.FileSource;
