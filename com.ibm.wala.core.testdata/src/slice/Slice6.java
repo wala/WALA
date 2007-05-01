82f285c616bf03919cad87c50ae71f6e6c0daeb8
sjfink <sjfink@f5eafffb-2e1d-0410-98e4-8ec43c5233c4>
2007-05-01 03:08:41 +0000

    another slicing test, from pavol cerny
    git-svn-id: https://wala.svn.sourceforge.net/svnroot/wala/trunk@1031 f5eafffb-2e1d-0410-98e4-8ec43c5233c4

---
+++ com.ibm.wala.core.testdata/src/slice/Slice6.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2007 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package slice;
+
+import java.util.Vector;
+
+public class Slice6 {
+  public static Vector<Integer> messages;
+
+  public static void main(String[] args) {
+
+    messages = new Vector<Integer>();
+    messages.add(5);
+
+    int message = (Integer) messages.elementAt(0);
+
+    sendMessage(message);
+  }
+
+  public static void sendMessage(int message) {
+    // do nothing
+  }
+}
