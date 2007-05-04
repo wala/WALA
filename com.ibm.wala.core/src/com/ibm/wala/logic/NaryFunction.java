23e2d805ac9e3c3bf2f7eb16a45f4ff5e1f120ce
schandra_sf <schandra_sf@f5eafffb-2e1d-0410-98e4-8ec43c5233c4>
2007-05-04 19:55:45 +0000

    Initial
    git-svn-id: https://wala.svn.sourceforge.net/svnroot/wala/trunk@1060 f5eafffb-2e1d-0410-98e4-8ec43c5233c4

---
+++ com.ibm.wala.core/src/com/ibm/wala/logic/NaryFunction.java
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * Copyright (c) 2006 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+package com.ibm.wala.logic;
+
+/**
+ * @author schandra_sf
+ */
+
+public class NaryFunction implements IFunction {
+
+	  private final String symbol;
+	  private final int k;
+
+	  private NaryFunction(String symbol, int k) {
+	    this.symbol = symbol;
+	    this.k = k;
+	  }
+
+	  public static NaryFunction make(String symbol, int k) {
+	    return new NaryFunction(symbol, k);
+	  }
+
+	  public int getNumberOfParameters() {
+	    return k;
+	  }
+
+	  public String getSymbol() {
+	    return symbol;
+	  }
+	  
+	  @Override
+	  public String toString() {
+	    return getSymbol() + " : int ^ k -> int";
+	  }
+}
