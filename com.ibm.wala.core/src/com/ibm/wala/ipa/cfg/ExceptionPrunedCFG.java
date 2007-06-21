2657dec4e7854866e8076dc804e900b706f940d7
dolby-oss <dolby-oss@f5eafffb-2e1d-0410-98e4-8ec43c5233c4>
2007-06-21 15:17:34 +0000

    moved ExceptionPrunedCFG to core
    git-svn-id: https://wala.svn.sourceforge.net/svnroot/wala/trunk@1304 f5eafffb-2e1d-0410-98e4-8ec43c5233c4

---
+++ com.ibm.wala.core/src/com/ibm/wala/ipa/cfg/ExceptionPrunedCFG.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2007 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package com.ibm.wala.ipa.cfg;
+
+import com.ibm.wala.cfg.*;
+
+public class ExceptionPrunedCFG {
+
+  private static final EdgeFilter exceptionEdgePruner = new EdgeFilter() {
+    public boolean hasNormalEdge(IBasicBlock src, IBasicBlock dst) {
+      return true;
+    }
+    public boolean hasExceptionalEdge(IBasicBlock src, IBasicBlock dst) {
+      return false;
+    }
+  };
+
+  public static PrunedCFG make(ControlFlowGraph cfg) {
+    return PrunedCFG.make(cfg, exceptionEdgePruner);
+  }
+}
+
