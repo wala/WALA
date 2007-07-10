2ec21016448a67887b55cde64fe90f09a5656550
sjfink <sjfink@f5eafffb-2e1d-0410-98e4-8ec43c5233c4>
2007-07-10 14:48:41 +0000

    new type inference test
    git-svn-id: https://wala.svn.sourceforge.net/svnroot/wala/trunk@1402 f5eafffb-2e1d-0410-98e4-8ec43c5233c4

---
+++ com.ibm.wala.core.testdata/src/typeInference/TI.java
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2007 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package typeInference;
+
+public class TI {
+  public static void foo() {
+    int[] x = new int[0];
+    System.out.println(x[0]);
+  }
+
+}
