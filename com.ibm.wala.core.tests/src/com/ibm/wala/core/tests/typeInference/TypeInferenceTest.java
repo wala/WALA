3cefd7fb6b1506ed71a42b2aa94925275230c9b3
sjfink <sjfink@f5eafffb-2e1d-0410-98e4-8ec43c5233c4>
2007-07-10 14:48:32 +0000

    new type inference test
    git-svn-id: https://wala.svn.sourceforge.net/svnroot/wala/trunk@1401 f5eafffb-2e1d-0410-98e4-8ec43c5233c4

---
+++ com.ibm.wala.core.tests/src/com/ibm/wala/core/tests/typeInference/TypeInferenceTest.java
@@ -0,0 +1,95 @@
+/*******************************************************************************
+ * Copyright (c) 2002 - 2006 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package com.ibm.wala.core.tests.typeInference;
+
+import java.util.Iterator;
+
+import com.ibm.wala.analysis.typeInference.TypeInference;
+import com.ibm.wala.classLoader.ClassLoaderFactory;
+import com.ibm.wala.classLoader.ClassLoaderFactoryImpl;
+import com.ibm.wala.classLoader.IMethod;
+import com.ibm.wala.core.tests.util.TestConstants;
+import com.ibm.wala.core.tests.util.WalaTestCase;
+import com.ibm.wala.emf.wrappers.EMFScopeWrapper;
+import com.ibm.wala.ipa.callgraph.AnalysisOptions;
+import com.ibm.wala.ipa.callgraph.AnalysisScope;
+import com.ibm.wala.ipa.callgraph.impl.Everywhere;
+import com.ibm.wala.ipa.cha.ClassHierarchy;
+import com.ibm.wala.ipa.cha.ClassHierarchyException;
+import com.ibm.wala.ssa.IR;
+import com.ibm.wala.ssa.SSAInstruction;
+import com.ibm.wala.types.MethodReference;
+import com.ibm.wala.util.Atom;
+import com.ibm.wala.util.ImmutableByteArray;
+import com.ibm.wala.util.UTF8Convert;
+import com.ibm.wala.util.graph.GraphIntegrity;
+import com.ibm.wala.util.graph.GraphIntegrity.UnsoundGraphException;
+import com.ibm.wala.util.warnings.Warnings;
+
+/**
+ * Test that the SSA-numbering of variables in the IR is deterministic.
+ * 
+ * Introduced 05-AUG-03; the default implementation of hashCode was being
+ * invoked. Object.hashCode is a source of random numbers and has no place in a
+ * deterministic program.
+ */
+public class TypeInferenceTest extends WalaTestCase {
+
+  private static final ClassLoader MY_CLASSLOADER = TypeInferenceTest.class.getClassLoader();
+
+  private AnalysisScope scope;
+
+  private ClassHierarchy cha;
+
+  private AnalysisOptions options;
+
+  public static void main(String[] args) {
+    justThisTest(TypeInferenceTest.class);
+  }
+
+  protected void setUp() throws Exception {
+
+    scope = new EMFScopeWrapper(TestConstants.WALA_TESTDATA, "J2SEClassHierarchyExclusions.xml", MY_CLASSLOADER);
+
+    options = new AnalysisOptions(scope, null);
+    ClassLoaderFactory factory = new ClassLoaderFactoryImpl(scope.getExclusions() );
+
+    try {
+      cha = ClassHierarchy.make(scope, factory);
+    } catch (ClassHierarchyException e) {
+      throw new Exception();
+    }
+  }
+
+  protected void tearDown() throws Exception {
+    Warnings.clear();
+    scope = null;
+    cha = null;
+    super.tearDown();
+  }
+
+  public void test1() {
+    MethodReference method = scope.findMethod(AnalysisScope.APPLICATION, "LtypeInference/TI", Atom.findOrCreateUnicodeAtom("foo"),
+        new ImmutableByteArray(UTF8Convert.toUTF8("()V")));
+    assertNotNull("method not found", method);
+    IMethod imethod = cha.resolveMethod(method);
+    assertNotNull("imethod not found", imethod);
+    IR ir = options.getIRFactory().makeIR(imethod, Everywhere.EVERYWHERE, options.getSSAOptions() );
+    System.out.println(ir);
+    
+    TypeInference ti = new TypeInference(ir);
+    ti.solve();
+    for (int i = 1; i< ir.getSymbolTable().getMaxValueNumber(); i++) {
+      System.err.println(i + " " + ti.getType(i));
+    }
+  }
+
+}
