7971640032a4a982ad36a868932f39add1c9597e
sjfink <sjfink@f5eafffb-2e1d-0410-98e4-8ec43c5233c4>
2007-07-16 17:52:46 +0000

    initial revision
    git-svn-id: https://wala.svn.sourceforge.net/svnroot/wala/trunk@1490 f5eafffb-2e1d-0410-98e4-8ec43c5233c4

---
+++ com.ibm.wala.core/src/com/ibm/wala/logic/NotFormulaMaxTerm.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2006 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package com.ibm.wala.logic;
+
+
+/**
+ * An irreducible Not Formula.
+ * 
+ * use carefully.
+ * 
+ * @author sjfink
+ */
+public class NotFormulaMaxTerm extends NotFormula implements IMaxTerm {
+
+  public static NotFormulaMaxTerm make(IFormula f) {
+    return new NotFormulaMaxTerm(f);
+  }
+
+  private NotFormulaMaxTerm(IFormula f) {
+    super(f);
+  }
+
+ 
+}
