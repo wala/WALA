0cb9c7e6299b44255cbbd34e760615deec8a7c8c
dolby-oss <dolby-oss@f5eafffb-2e1d-0410-98e4-8ec43c5233c4>
2007-09-13 15:09:57 +0000

    extensions to source java slicer
    git-svn-id: https://wala.svn.sourceforge.net/svnroot/wala/trunk@1746 f5eafffb-2e1d-0410-98e4-8ec43c5233c4

--- com.ibm.wala.cast.java/src/com/ibm/wala/cast/java/ipa/slicer/AstJavaSlicer.java
+++ com.ibm.wala.cast.java/src/com/ibm/wala/cast/java/ipa/slicer/AstJavaSlicer.java
@@ -1,13 +1,18 @@
 package com.ibm.wala.cast.java.ipa.slicer;
 
-import java.util.Collection;
+import java.util.*;
 
+import com.ibm.wala.cast.ir.ssa.*;
 import com.ibm.wala.cast.java.ipa.modref.AstJavaModRef;
-import com.ibm.wala.ipa.callgraph.CallGraph;
+import com.ibm.wala.cast.java.ipa.slicer.AstJavaSlicer;
+import com.ibm.wala.ipa.callgraph.*;
+import com.ibm.wala.ipa.callgraph.impl.*;
 import com.ibm.wala.ipa.callgraph.propagation.PointerAnalysis;
-import com.ibm.wala.ipa.slicer.SDG;
-import com.ibm.wala.ipa.slicer.Slicer;
-import com.ibm.wala.ipa.slicer.Statement;
+import com.ibm.wala.ipa.slicer.*;
+import com.ibm.wala.ssa.*;
+import com.ibm.wala.util.collections.*;
+import com.ibm.wala.util.debug.*;
+import com.ibm.wala.util.graph.traverse.*;
 
 public class AstJavaSlicer extends Slicer {
 
@@ -26,6 +31,33 @@ public class AstJavaSlicer extends Slicer {
   public static Collection<Statement> computeSlice(SDG sdg, Collection<Statement> ss, CallGraph cg, PointerAnalysis pa,
       DataDependenceOptions dOptions, ControlDependenceOptions cOptions, boolean backward) {
     return computeSlice(sdg, ss, cg, pa, new AstJavaModRef(), dOptions, cOptions, backward);
+  }
+
+  public static Set gatherAssertions(CallGraph CG, Collection partialRoots) {
+    Set result = new HashSet();
+    for(Iterator ns = DFS.getReachableNodes(CG, partialRoots).iterator();
+	ns.hasNext(); )
+    {
+      CGNode n = (CGNode) ns.next();
+      IR nir = n.getIR();
+      SSAInstruction insts[] = nir.getInstructions();
+      for(int i = 0; i < insts.length; i++) {
+	if (insts[i] instanceof AstAssertInstruction) {
+	  result.add( new NormalStatement(n, i) );
+	}
+      }
+    }
+
+    return result;
+  } 
+
+  public static Pair<Collection<Statement>, SDG> 
+    computeAssertionSlice(CallGraph CG, PointerAnalysis pa, Collection partialRoots)
+  {
+    CallGraph pcg = PartialCallGraph.make(CG, new LinkedHashSet(partialRoots));
+    SDG sdg = new SDG(pcg, pa, new AstJavaModRef(), DataDependenceOptions.FULL, ControlDependenceOptions.FULL);
+    Trace.println("SDG:\n" + sdg);
+    return Pair.make(AstJavaSlicer.computeBackwardSlice(sdg, gatherAssertions(CG, partialRoots), pcg, pa, DataDependenceOptions.FULL, ControlDependenceOptions.FULL), sdg);
   }
 
 }
