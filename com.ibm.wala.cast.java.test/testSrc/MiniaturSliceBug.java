8d870a917c3c779de4c2ce2d1d29b34fc2b7ba14
dolby-oss <dolby-oss@f5eafffb-2e1d-0410-98e4-8ec43c5233c4>
2007-09-17 21:05:09 +0000

    add missing files
    git-svn-id: https://wala.svn.sourceforge.net/svnroot/wala/trunk@1761 f5eafffb-2e1d-0410-98e4-8ec43c5233c4

---
+++ com.ibm.wala.cast.java.test/testSrc/MiniaturSliceBug.java
@@ -0,0 +1,73 @@
+abstract class PrimitiveWrapper {
+    
+  /**
+   * Sets the integer representation of the underlying primitive
+   * to the given value.
+   * @effects this.intVal' = i
+   */
+  public abstract void setIntValue(int i);
+    
+  /**
+   * Returns the integer representation of the underlying primitive.
+   * @return this.intVal
+   */
+  public abstract int intValue();
+	
+  /**
+   * Returns true if this and the given object are 
+   * pri
+   * {@inheritDoc}
+   * @see java.lang.Object#equals(java.lang.Object)
+   */
+  abstract public boolean equals(Object o);
+}
+
+final class IntWrapper extends PrimitiveWrapper {
+  private int val;
+	
+  /**
+   * Constructs a wrapper for the given int.
+   * @effects this.intVal' = val
+   */
+  public IntWrapper(int val) { 
+    this.val = val;
+  }
+	
+  /**
+   * {@inheritDoc}
+   * @see com.ibm.miniatur.tests.sequential.PrimitiveWrapper#intValue()
+   */
+  public int intValue() {
+    return val;
+  }
+
+  /**
+   * {@inheritDoc}
+   * @see com.ibm.miniatur.tests.sequential.PrimitiveWrapper#setIntValue(int)
+   */
+  public void setIntValue(int i) {
+    this.val = i;
+  }
+	
+  /**
+   * {@inheritDoc}
+   * @see com.ibm.miniatur.tests.sequential.PrimitiveWrapper#equals(java.lang.Object)
+   */
+  public boolean equals(Object o) { 
+    return o instanceof IntWrapper && ((IntWrapper)o).val==val;
+  }
+}
+
+public class MiniaturSliceBug {
+
+  public void validNonDispatchedCall(IntWrapper wrapper) { 
+    wrapper.setIntValue(3);
+    assert wrapper.intValue() == 3;
+    wrapper.equals(wrapper);
+  }
+
+  public static void main(String[] args) {
+    (new MiniaturSliceBug()).validNonDispatchedCall(new IntWrapper(-1));
+  }
+
+}
