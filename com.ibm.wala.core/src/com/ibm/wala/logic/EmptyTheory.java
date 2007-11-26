2612c1b3432aecc23878fb4b557c52ef769c73ff
sjfink <sjfink@f5eafffb-2e1d-0410-98e4-8ec43c5233c4>
2007-11-26 15:25:23 +0000

    initial revision
    git-svn-id: https://wala.svn.sourceforge.net/svnroot/wala/trunk@2069 f5eafffb-2e1d-0410-98e4-8ec43c5233c4

---
+++ com.ibm.wala.core/src/com/ibm/wala/logic/EmptyTheory.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright (c) 2007 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package com.ibm.wala.logic;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class EmptyTheory extends AbstractTheory {
+
+  private final static EmptyTheory INSTANCE = new EmptyTheory();
+  
+  public static EmptyTheory singleton() {
+    return INSTANCE;
+  }
+
+  private EmptyTheory() {}
+  
+  @Override
+  public Collection<? extends IFormula> getSentences() {
+    return Collections.emptySet();
+  }
+
+  @Override
+  public IVocabulary getVocabulary() {
+    return BasicVocabulary.make(Collections.emptySet());
+  }
+
+}
