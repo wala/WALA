5dc5550b532fa4b69a5d0bcbd86b58c5b88f9bc5
sjfink <sjfink@f5eafffb-2e1d-0410-98e4-8ec43c5233c4>
2007-12-03 15:42:35 +0000

    clean up model of arraycopy
    git-svn-id: https://wala.svn.sourceforge.net/svnroot/wala/trunk@2086 f5eafffb-2e1d-0410-98e4-8ec43c5233c4

--- com.ibm.wala.core/src/com/ibm/wala/model/java/lang/System.java
+++ com.ibm.wala.core/src/com/ibm/wala/model/java/lang/System.java
@@ -26,82 +26,51 @@ public class System {
       return;
     }
     
-    try {
+    if (src instanceof Object[]) {
       Object[] A = (Object[]) src;
       Object[] B = (Object[]) dest;
       for (int i = 0; i < A.length; i++)
         B[i] = A[i];
-    } catch (ClassCastException cce) {
-    }
-
-    try {
+    } else if (src instanceof int[]) {
       int[] A = (int[]) src;
       int[] B = (int[]) dest;
       for (int i = 0; i < A.length; i++)
         B[i] = A[i];
-    } catch (ClassCastException cce) {
-    }
-    
-    try {
+    } else if (src instanceof char[]) {
       char[] A = (char[]) src;
       char[] B = (char[]) dest;
       for (int i = 0; i < A.length; i++)
         B[i] = A[i];
-    } catch (ClassCastException cce) {
-    }
-    
-    try {
+    } else if (src instanceof short[]) {
       short[] A = (short[]) src;
       short[] B = (short[]) dest;
       for (int i = 0; i < A.length; i++)
         B[i] = A[i];
-    } catch (ClassCastException cce) {
-    }
-    
-    try {
+    } else if (src instanceof long[]) {
       long[] A = (long[]) src;
       long[] B = (long[]) dest;
       for (int i = 0; i < A.length; i++)
         B[i] = A[i];
-    } catch (ClassCastException cce) {
-    }
-    
-    try {
+    } else if (src instanceof byte[]) {
       byte[] A = (byte[]) src;
       byte[] B = (byte[]) dest;
       for (int i = 0; i < A.length; i++)
         B[i] = A[i];
-    } catch (ClassCastException cce) {
-    }
-    
-    try {
+    } else if (src instanceof double[]) {
       double[] A = (double[]) src;
       double[] B = (double[]) dest;
       for (int i = 0; i < A.length; i++)
         B[i] = A[i];
-    } catch (ClassCastException cce) {
-    }
-    
-    try {
+    } else if (src instanceof boolean[]) {
       boolean[] A = (boolean[]) src;
       boolean[] B = (boolean[]) dest;
       for (int i = 0; i < A.length; i++)
         B[i] = A[i];
-    } catch (ClassCastException cce) {
-    }
-    
-    try {
+    } else if (src instanceof float[]) {
       float[] A = (float[]) src;
       float[] B = (float[]) dest;
       for (int i = 0; i < A.length; i++)
         B[i] = A[i];
-    } catch (ClassCastException cce) {
     }
   }
-
-//  public static void main(String[] args) {
-//    char[] src1 = new char[] { 'a' , 'b' };
-//    char[] dest1 = new char[] { 'c', 'd' };
-//    arraycopy(src1, dest1);
-//  }
 }
