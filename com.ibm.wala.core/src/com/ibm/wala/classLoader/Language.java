da1ac21d122a46ac149cd4472af5d54e06763172
rmfuhrer <rmfuhrer@f5eafffb-2e1d-0410-98e4-8ec43c5233c4>
2007-12-06 14:31:45 +0000

    Added API to Language for representing the hierarchy of languages, which is necessary for WALA ClassLoaders to correlate loaders to Languages and know which languages share a ClassHierarchy.
    git-svn-id: https://wala.svn.sourceforge.net/svnroot/wala/trunk@2110 f5eafffb-2e1d-0410-98e4-8ec43c5233c4

--- com.ibm.wala.core/src/com/ibm/wala/classLoader/Language.java
+++ com.ibm.wala.core/src/com/ibm/wala/classLoader/Language.java
@@ -1,20 +1,25 @@
 package com.ibm.wala.classLoader;
 
+import java.util.Set;
+
 import com.ibm.wala.types.ClassLoaderReference;
 import com.ibm.wala.types.TypeReference;
 import com.ibm.wala.util.Atom;
 
 public interface Language {
 
-  public static Language JAVA = new Language() {
+  public static Language JAVA = new LanguageImpl() {
+    @Override
     public Atom getName() {
       return ClassLoaderReference.Java;
     }
 
+    @Override
     public TypeReference getRootType() {
       return TypeReference.JavaLangObject;
     }
 
+    @Override
     public TypeReference getConstantType(Object o) {
       if (o instanceof String) {
         return TypeReference.JavaLangString;
@@ -23,12 +28,19 @@ public interface Language {
       }
     }
 
+    @Override
     public boolean isNullType(TypeReference type) {
       return false;
     }
   };
 
   Atom getName();
+
+  Language getBaseLanguage();
+
+  void registerDerivedLanguage(Language l);
+
+  Set<Language> getDerivedLanguages();
 
   TypeReference getRootType();
 
