7278cf71ff9941f513b09de5c4c4c01044735214
rmfuhrer <rmfuhrer@f5eafffb-2e1d-0410-98e4-8ec43c5233c4>
2007-12-06 14:32:59 +0000

    Extracted base Language implementation to a separate class for better reuse.
    git-svn-id: https://wala.svn.sourceforge.net/svnroot/wala/trunk@2111 f5eafffb-2e1d-0410-98e4-8ec43c5233c4

---
+++ com.ibm.wala.core/src/com/ibm/wala/classLoader/LanguageImpl.java
@@ -0,0 +1,61 @@
+/*******************************************************************************
+ * Copyright (c) 2007 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package com.ibm.wala.classLoader;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public abstract class LanguageImpl implements Language {
+
+  private Language baseLang;
+
+  private Set<Language> derivedLangs= new HashSet<Language>();
+
+  public LanguageImpl() { }
+
+  public LanguageImpl(Language base) {
+    baseLang= base;
+    base.registerDerivedLanguage(this);
+  }
+
+  public Language getBaseLanguage() {
+    return baseLang;
+  }
+
+  public Set<Language> getDerivedLanguages() {
+    return derivedLangs;
+  }
+
+  public void registerDerivedLanguage(Language l) {
+    derivedLangs.add(l);
+    if (baseLang != null)
+      baseLang.registerDerivedLanguage(l);
+  }
+
+  @Override
+  public int hashCode() {
+    return 1609 + 199 * getName().hashCode();
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (!(o instanceof LanguageImpl))
+      return false;
+    LanguageImpl other= (LanguageImpl) o;
+
+    return getName().equals(other.getName());
+  }
+
+  @Override
+  public String toString() {
+    return getName().toString();
+  }
+}
