72ba229e6c67d08e2d43ed47a79a914b8900307a
sjfink <sjfink@f5eafffb-2e1d-0410-98e4-8ec43c5233c4>
2007-12-11 16:38:22 +0000

    clean up header comment
    git-svn-id: https://wala.svn.sourceforge.net/svnroot/wala/trunk@2156 f5eafffb-2e1d-0410-98e4-8ec43c5233c4

--- com.ibm.wala.cast/source/java/com/ibm/wala/cast/ipa/callgraph/CAstAnalysisScope.java
+++ com.ibm.wala.cast/source/java/com/ibm/wala/cast/ipa/callgraph/CAstAnalysisScope.java
@@ -6,13 +6,7 @@
  * 
  * Contributors: IBM Corporation - initial API and implementation
  ******************************************************************************/
-// Licensed Materials - Property of IBM
-// 5724-D15
-// (C) Copyright IBM Corporation 2004. All Rights Reserved.
-// Note to U.S. Government Users Restricted Rights: Use, duplication or
-// disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
-//                                                                             
-// ---------------------------------------------------------------------------
+
 package com.ibm.wala.cast.ipa.callgraph;
 
 import java.io.File;
