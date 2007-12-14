568faedb1bd0145bc93384ea9a059fbb51158943
sjfink <sjfink@f5eafffb-2e1d-0410-98e4-8ec43c5233c4>
2007-12-14 14:30:07 +0000

    fix bug [ 1850456 ] error "I is null" when slicing
    git-svn-id: https://wala.svn.sourceforge.net/svnroot/wala/trunk@2184 f5eafffb-2e1d-0410-98e4-8ec43c5233c4

---
+++ com.ibm.wala.core.testdata/src/slice/TestMessageFormat.java
@@ -0,0 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 2007 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package slice;
+
+import java.text.MessageFormat;
+
+public class TestMessageFormat {
+
+  public static void main(String args[]) {
+    MessageFormat.format(null, (Object[])null);
+  }
+}
