95893ab9e321dc6ecc0c0fd1123544acb95bfd71
sjfink <sjfink@f5eafffb-2e1d-0410-98e4-8ec43c5233c4>
2008-01-17 22:51:42 +0000

    initial revision
    git-svn-id: https://wala.svn.sourceforge.net/svnroot/wala/trunk@2415 f5eafffb-2e1d-0410-98e4-8ec43c5233c4

---
+++ com.ibm.wala.core/src/com/ibm/wala/util/collections/OrFilter.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright (c) 2007 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package com.ibm.wala.util.collections;
+
+/**
+ * A filter "A or B"
+ * 
+ * @author sjfink
+ */
+public class OrFilter<T> implements Filter<T> {
+
+  public static <T> OrFilter<T> createOrFilter(Filter<T> a, Filter<T> b) {
+    assert a != null;
+    assert b != null;
+    return new OrFilter<T>(a, b);
+  }
+
+  private final Filter<T> a;
+  private final Filter<T> b;
+  
+  private OrFilter(Filter<T> a, Filter<T> b) {
+    this.a = a;
+    this.b = b;
+  }
+  
+  public boolean accepts(T o) {
+    return a.accepts(o) || b.accepts(o);
+  }
+
+}
