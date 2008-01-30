2fc1dd344219deb5d436b9358f9401c2aac07f45
msridhar1 <msridhar1@f5eafffb-2e1d-0410-98e4-8ec43c5233c4>
2008-01-30 19:47:00 +0000

    add NO_HEAP_NO_EXCEPTIONS data dependence option
    git-svn-id: https://wala.svn.sourceforge.net/svnroot/wala/trunk@2495 f5eafffb-2e1d-0410-98e4-8ec43c5233c4

--- com.ibm.wala.core/src/com/ibm/wala/ipa/slicer/Slicer.java
+++ com.ibm.wala.core/src/com/ibm/wala/ipa/slicer/Slicer.java
@@ -63,6 +63,7 @@ public class Slicer {
     NO_BASE_NO_EXCEPTIONS("no_base_no_exceptions", true, false, false, true),
     NO_BASE_NO_HEAP_NO_EXCEPTIONS("no_base_no_heap_no_exceptions", true, true, false, true),
     NO_HEAP("no_heap", false, true, false, false),
+    NO_HEAP_NO_EXCEPTIONS("no_heap_no_exceptions", false, true, false, true),
     NONE("none", true, true, true, true),
     REFLECTION("no_base_no_heap_no_cast", true, true, true, true);
 
