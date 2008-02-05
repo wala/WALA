518d21140ebd3b72bcd0856dd3aec43bb69bf58e
sjfink <sjfink@f5eafffb-2e1d-0410-98e4-8ec43c5233c4>
2008-02-05 21:21:19 +0000

    bug fixes and new tests for new Class.forName processing.
    git-svn-id: https://wala.svn.sourceforge.net/svnroot/wala/trunk@2543 f5eafffb-2e1d-0410-98e4-8ec43c5233c4

---
+++ com.ibm.wala.core.testdata/src/reflection/Reflect2.java
@@ -0,0 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2008 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package reflection;
+
+public class Reflect2 {
+
+  public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+    Class.forName("java.lang.Integer");
+  }
+}
