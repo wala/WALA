ee6e5ca941aba6e92f31753420abdd79cc9bf57e
pistoia <pistoia@f5eafffb-2e1d-0410-98e4-8ec43c5233c4>
2008-02-25 20:38:11 +0000

    removed SecurityException from the throws clause
    git-svn-id: https://wala.svn.sourceforge.net/svnroot/wala/trunk@2615 f5eafffb-2e1d-0410-98e4-8ec43c5233c4

--- com.ibm.wala.core.testdata/src/reflection/Reflect8.java
+++ com.ibm.wala.core.testdata/src/reflection/Reflect8.java
@@ -8,8 +8,8 @@ import java.lang.reflect.InvocationTargetException;
  */
 public class Reflect8 {
   @SuppressWarnings("unchecked")
-  public static void main(String[] args) throws ClassNotFoundException, SecurityException, NoSuchMethodException,
-      IllegalArgumentException, InstantiationException, IllegalAccessException, InvocationTargetException {
+  public static void main(String[] args) throws ClassNotFoundException, IllegalArgumentException, InstantiationException,
+      IllegalAccessException, InvocationTargetException {
     Class c = Class.forName("java.lang.Integer");
     Constructor[] ctors = c.getConstructors();
     Integer i = (Integer) ctors[0].newInstance(new Integer(1));
