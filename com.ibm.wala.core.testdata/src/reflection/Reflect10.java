e48a066a70e10f9dc6c5914c8f764ba9cfd75ca9
sjfink <sjfink@f5eafffb-2e1d-0410-98e4-8ec43c5233c4>
2008-02-26 15:24:33 +0000

    implement getMethods()
    git-svn-id: https://wala.svn.sourceforge.net/svnroot/wala/trunk@2627 f5eafffb-2e1d-0410-98e4-8ec43c5233c4

---
+++ com.ibm.wala.core.testdata/src/reflection/Reflect10.java
@@ -0,0 +1,17 @@
+package reflection;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * Test of Method.invoke
+ */
+public class Reflect10 {
+  @SuppressWarnings("unchecked")
+  public static void main(String[] args) throws ClassNotFoundException, IllegalArgumentException, InstantiationException,
+      IllegalAccessException, InvocationTargetException, SecurityException, NoSuchMethodException {
+    Class c = Class.forName("java.lang.Integer");
+    Method[] m = c.getMethods();
+    m[0].invoke(new Integer(2), new Object[] {});
+  }
+}
