f604add130e609764d7c3339455986f9cba4bb8c
sjfink <sjfink@f5eafffb-2e1d-0410-98e4-8ec43c5233c4>
2008-03-02 01:53:55 +0000

    bug fix
    git-svn-id: https://wala.svn.sourceforge.net/svnroot/wala/trunk@2643 f5eafffb-2e1d-0410-98e4-8ec43c5233c4

--- com.ibm.wala.core/src/com/ibm/wala/analysis/reflection/ReflectiveInvocationSelector.java
+++ com.ibm.wala.core/src/com/ibm/wala/analysis/reflection/ReflectiveInvocationSelector.java
@@ -26,8 +26,8 @@ import com.ibm.wala.ssa.SymbolTable;
 import com.ibm.wala.types.TypeReference;
 
 /**
- * A {@link ContextSelector} to intercept calls to reflective method invocations such as
- * Constructor.newInstance and Method.invoke
+ * A {@link ContextSelector} to intercept calls to reflective method invocations such as Constructor.newInstance and
+ * Method.invoke
  * 
  * @author pistoia
  * @author sjfink
@@ -48,18 +48,16 @@ class ReflectiveInvocationSelector implements ContextSelector {
   /**
    * Creates a callee target based on the following criteria:
    * <ol>
-   * <li> If the method being invoked through reflection is definitely static, then
-   *      do not create a callee target for any <code>callee</code> method that is not static.
-   *      In this case, return <code>null</code>.
-   * <li> If the method being invoked through reflection takes a constant number of
-   *      parameters, <code>n</code>, then do not create a callee target for any
-   *      <code>callee</code> method that takes a number of parameters different from
-   *      <code>n</code>.  In this case, return <code>null</code>.
+   * <li> If the method being invoked through reflection is definitely static, then do not create a callee target for
+   * any <code>callee</code> method that is not static. In this case, return <code>null</code>.
+   * <li> If the method being invoked through reflection takes a constant number of parameters, <code>n</code>, then
+   * do not create a callee target for any <code>callee</code> method that takes a number of parameters different from
+   * <code>n</code>. In this case, return <code>null</code>.
    * <li> Otherwise, return a new {@link ReceiverInstanceContext} for <code>receiver</code>.
-   * </ol>       
+   * </ol>
    */
   public Context getCalleeTarget(CGNode caller, CallSiteReference site, IMethod callee, InstanceKey receiver) {
-    if (! mayUnderstand(caller, site, callee, receiver)) {
+    if (!mayUnderstand(caller, site, callee, receiver)) {
       return null;
     }
     IR ir = caller.getIR();
@@ -72,16 +70,16 @@ class ReflectiveInvocationSelector implements ContextSelector {
     IMethod m = (IMethod) receiverConstantKey.getValue();
     boolean isStatic = m.isStatic();
     boolean isConstructor = isConstructorConstant(receiver);
-    
+
     // If the method being invoked through reflection is not a constructor and is definitely static, then
     // we should not create a callee target for any method that is not static
-    if (! isConstructor) {
+    if (!isConstructor) {
       int recvUse = invokeInstructions[0].getUse(1);
-      if (st.isNullConstant(recvUse) && ! isStatic) {
+      if (st.isNullConstant(recvUse) && !isStatic) {
         return null;
       }
     }
-    
+
     // If the method being invoked through reflection is being passed n parameters,
     // then we should not create a callee target for any method that takes a number
     // of parameters different from n
@@ -91,12 +89,12 @@ class ReflectiveInvocationSelector implements ContextSelector {
     // is an array of objects corresponding to the parameters passed to the method.
     int paramIndex = isConstructor ? 1 : 2;
     int paramUse = invokeInstructions[0].getUse(paramIndex);
-    SSAInstruction instr = caller.getDU().getDef(paramUse);    
-    if (! (instr instanceof SSANewInstruction)) {
+    SSAInstruction instr = caller.getDU().getDef(paramUse);
+    if (!(instr instanceof SSANewInstruction)) {
       return new ReceiverInstanceContext(receiver);
     }
     SSANewInstruction newInstr = (SSANewInstruction) instr;
-    if (! newInstr.getConcreteType().isArrayType()) {
+    if (!newInstr.getConcreteType().isArrayType()) {
       return null;
     }
     int vn = newInstr.getUse(0);
@@ -116,13 +114,15 @@ class ReflectiveInvocationSelector implements ContextSelector {
    * This object may understand a dispatch to Constructor.newInstance().
    */
   public boolean mayUnderstand(CGNode caller, CallSiteReference site, IMethod targetMethod, InstanceKey instance) {
-    if (targetMethod.getReference().equals(ReflectiveInvocationInterpreter.METHOD_INVOKE) ||
-        isConstructorConstant(instance) && targetMethod.getReference().equals(ReflectiveInvocationInterpreter.CTOR_NEW_INSTANCE)) {
+    if (instance instanceof ConstantKey) {
+      if (targetMethod.getReference().equals(ReflectiveInvocationInterpreter.METHOD_INVOKE) || isConstructorConstant(instance)
+          && targetMethod.getReference().equals(ReflectiveInvocationInterpreter.CTOR_NEW_INSTANCE)) {
         return true;
+      }
     }
     return false;
   }
-  
+
   private boolean isConstructorConstant(InstanceKey instance) {
     if (instance instanceof ConstantKey) {
       ConstantKey c = (ConstantKey) instance;
