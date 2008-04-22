c10084093a445bf3e17dfd7bc8ad84eb4d94ece7
sjfink <sjfink@f5eafffb-2e1d-0410-98e4-8ec43c5233c4>
2008-04-22 12:15:03 +0000

    initial check-in
    git-svn-id: https://wala.svn.sourceforge.net/svnroot/wala/trunk@2763 f5eafffb-2e1d-0410-98e4-8ec43c5233c4

---
+++ com.ibm.wala.core.tests/src/com/ibm/wala/core/tests/basic/OrdinalSetTest.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2008 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package com.ibm.wala.core.tests.basic;
+
+import com.ibm.wala.core.tests.util.WalaTestCase;
+import com.ibm.wala.util.intset.OrdinalSet;
+
+/**
+ * JUnit tests for some {@link OrdinalSet} operations.
+ * 
+ * @author sfink
+ */
+public class OrdinalSetTest extends WalaTestCase {
+
+  public void test1() {
+    OrdinalSet.unify(OrdinalSet.empty(), OrdinalSet.empty());
+  }
+}
