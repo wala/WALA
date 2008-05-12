ba8283d08750d0eeb12b5e8b3abf1dfdd8073cfb
sjfink <sjfink@f5eafffb-2e1d-0410-98e4-8ec43c5233c4>
2008-05-12 00:26:43 +0000

    initial revision
    git-svn-id: https://wala.svn.sourceforge.net/svnroot/wala/trunk@2795 f5eafffb-2e1d-0410-98e4-8ec43c5233c4

---
+++ com.ibm.wala.core/src/com/ibm/wala/dataflow/IFDS/BoundedTabulationSolver.java
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * Copyright (c) 2008 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package com.ibm.wala.dataflow.IFDS;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+
+/**
+ * A {@link TabulationSolver} that gives up after a finite bound.
+ * 
+ * @author sjfink
+ *
+ */
+public class BoundedTabulationSolver<T, P> extends TabulationSolver<T, P> {
+
+  public static <T, P> BoundedTabulationSolver<T, P> createBoundedTabulationSolver(TabulationProblem<T, P> p, int bound,
+      IProgressMonitor monitor) {
+    return new BoundedTabulationSolver<T, P>(p, bound, monitor);
+  }
+
+  private final int bound;
+  
+  private int numSteps = 0;
+  
+  protected BoundedTabulationSolver(TabulationProblem<T, P> p, int bound, IProgressMonitor monitor) {
+    super(p, monitor);
+    this.bound = bound;
+  }
+  
+  @Override
+  protected void propagate(T s_p, int i,T n, int j) {
+    if (numSteps < bound) {
+      numSteps++;
+      super.propagate(s_p, i, n, j);
+    }
+  }
+  
+  public int getNumSteps() {
+    return numSteps;
+  }
+}
