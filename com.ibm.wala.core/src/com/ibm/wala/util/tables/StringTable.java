52f75992708c63e685266573a67142518ebf8578
sjfink <sjfink@f5eafffb-2e1d-0410-98e4-8ec43c5233c4>
2008-06-29 11:53:27 +0000

    tweak Tables API with comment characters
    git-svn-id: https://wala.svn.sourceforge.net/svnroot/wala/trunk@2927 f5eafffb-2e1d-0410-98e4-8ec43c5233c4

--- com.ibm.wala.core/src/com/ibm/wala/util/tables/StringTable.java
+++ com.ibm.wala.core/src/com/ibm/wala/util/tables/StringTable.java
@@ -53,44 +53,43 @@ public class StringTable extends Table<String> implements Cloneable {
 
   /**
    * read from a direct (native) text file
-   * @throws IOException 
-   * @throws FileNotFoundException 
-   * @throws IllegalArgumentException  if fileName is null
+   * 
+   * @throws IOException
+   * @throws FileNotFoundException
+   * @throws IllegalArgumentException if fileName is null
    * 
    */
-  public static StringTable readFromDirectTextFile(String fileName) throws FileNotFoundException, IOException{
+  public static StringTable readFromDirectTextFile(String fileName, Character comment) throws FileNotFoundException, IOException {
     if (fileName == null) {
       throw new IllegalArgumentException("fileName is null");
     }
     File f = new File(fileName);
-    return readFromTextFile(f);
+    return readFromTextFile(f, comment);
   }
 
   /**
    * read from a text file obtained as a resource
    */
-  public static StringTable readFromTextFile(String fileName) throws IOException {
+  public static StringTable readFromTextFile(String fileName, Character comment) throws IOException {
     File f = FileProvider.getFile(fileName);
-    return readFromTextFile(f);
+    return readFromTextFile(f, comment);
   }
 
   /**
-   * @param f
-   *          a file containing a table in text format, whitespace delimited
-   * @throws IOException 
-   * @throws FileNotFoundException 
+   * @param f a file containing a table in text format, whitespace delimited
+   * @throws IOException
+   * @throws FileNotFoundException
    */
-  public static StringTable readFromTextFile(File f) throws FileNotFoundException, IOException {
-    return readFromStream(new FileInputStream(f));
+  public static StringTable readFromTextFile(File f, Character comment) throws FileNotFoundException, IOException {
+    return readFromStream(new FileInputStream(f), comment);
   }
 
   /**
-   * @param s
-   *          a stream containing a table in text format, whitespace delimited
-   * @throws IOException 
-   * @throws IllegalArgumentException  if s is null
+   * @param s a stream containing a table in text format, whitespace delimited
+   * @throws IOException
+   * @throws IllegalArgumentException if s is null
    */
-  public static StringTable readFromStream(InputStream s) throws IOException {
+  public static StringTable readFromStream(InputStream s, Character commentToken) throws IOException {
     if (s == null) {
       throw new IllegalArgumentException("s is null");
     }
@@ -100,46 +99,49 @@ public class StringTable extends Table<String> implements Cloneable {
 
     // LineNumberReader reader = new LineNumberReader(new
     // InputStreamReader(new FileInputStream(f)));
-    String line = readNextNonCommentLine(reader);
+    String line = readNextNonCommentLine(reader, commentToken);
     if (line == null) {
       throw new IOException("first line expected to be column headings");
     }
     result.populateColumnHeadings(line);
 
-    line = readNextNonCommentLine(reader);
+    line = readNextNonCommentLine(reader, commentToken);
     int row = 0;
     while (line != null) {
       result.populateRow(row, line);
-      line = readNextNonCommentLine(reader);
+      line = readNextNonCommentLine(reader, commentToken);
       row++;
     }
 
     return result;
   }
 
-  public static String readNextNonCommentLine(LineNumberReader reader) throws IOException {
+  public static String readNextNonCommentLine(LineNumberReader reader, Character commentToken) throws IOException {
     if (reader == null) {
       throw new IllegalArgumentException("reader is null");
     }
     String line = reader.readLine();
-    while (line != null && isCommented(line)) {
+    while (line != null && isCommented(line, commentToken)) {
       line = reader.readLine();
     }
     return line;
   }
 
-  private static boolean isCommented(String line) {
+  private static boolean isCommented(String line, Character commentToken) {
     if (line.length() == 0) {
       return true;
     }
-    return line.charAt(0) == Constants.COMMENT;
+    if (commentToken == null) {
+      return false;
+    }
+    return line.charAt(0) == commentToken;
   }
 
   private void populateRow(int row, String line) {
     StringTokenizer st = new StringTokenizer(line);
     int nColumns = st.countTokens();
     Assertions.productionAssertion(nColumns == getNumberOfColumns(), "expected " + getNumberOfColumns() + " got " + nColumns
-        + " row " + row);
+        + " row " + row + " " + line.length());
     SimpleVector<String> r = new SimpleVector<String>();
     rows.add(row, r);
     for (int i = 0; i < nColumns; i++) {
@@ -148,8 +150,7 @@ public class StringTable extends Table<String> implements Cloneable {
   }
 
   /**
-   * @param line
-   *          a whitespace-delimited string of column names
+   * @param line a whitespace-delimited string of column names
    */
   private void populateColumnHeadings(String line) {
     StringTokenizer st = new StringTokenizer(line);
