59c6cac3818945b8843c6d3a48fd037357597a7a
sjfink <sjfink@f5eafffb-2e1d-0410-98e4-8ec43c5233c4>
2008-08-15 14:49:41 +0000

    support for reading InnerClass attributes
    git-svn-id: https://wala.svn.sourceforge.net/svnroot/wala/trunk@2991 f5eafffb-2e1d-0410-98e4-8ec43c5233c4

--- com.ibm.wala.shrike/src/com/ibm/wala/shrikeCT/InnerClassesReader.java
+++ com.ibm.wala.shrike/src/com/ibm/wala/shrikeCT/InnerClassesReader.java
@@ -36,4 +36,52 @@ public final class InnerClassesReader extends AttributeReader {
     }
     return r;
   }
+  
+  /**
+   * @return the names of inner classes this attribute holds information about.
+   */
+  public String[] getInnerClasses() throws InvalidClassFileException {
+    int count = cr.getUShort(attr + 6);
+    String[] r = new String[count];
+    ConstantPoolParser cp = cr.getCP();
+    for (int i = 0; i < r.length; i++) {
+      r[i] = cp.getCPClass(cr.getUShort(attr + 8 + i * 8));
+    }
+    return r;
+  }
+
+  /**
+   * return the name of the outer class recorded as the enclosing class for a class named s.
+   * return null if not found.
+   * @throws InvalidClassFileException 
+   */
+  public String getOuterClass(String s) throws InvalidClassFileException {
+    String[] inner = getInnerClasses();
+    for (int i =0; i < inner.length; i++) {
+      if (inner[i].equals(s)) {
+        int x = cr.getUShort(attr + 8 + i*8 + 2);
+        if (x != 0) {
+          ConstantPoolParser cp = cr.getCP();
+          return cp.getCPClass(cr.getUShort(attr + 8 + i * 8 + 2));
+        }
+      }
+    }
+    return null;
+  }
+  
+  /**
+   * return the mask of flags recorded in the InnerClasses attribute for a class named s.
+   * return 0 if not found.
+   * @throws InvalidClassFileException 
+   */
+  public int getAccessFlags(String s) throws InvalidClassFileException {
+    String[] inner = getInnerClasses();
+    for (int i =0; i < inner.length; i++) {
+      if (inner[i].equals(s)) {
+        return cr.getUShort(attr + 8 + i*8 + 6);
+      }
+    }
+    return 0;
+  }
+  
 }
