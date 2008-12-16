6fe59a49fd38661954aab4a3181f01a6de4a21bd
dolby-oss <dolby-oss@f5eafffb-2e1d-0410-98e4-8ec43c5233c4>
2008-12-16 15:19:56 +0000

    move CAst Java test data to separate project to ease use in plugin tests
    git-svn-id: https://wala.svn.sourceforge.net/svnroot/wala/trunk@3140 f5eafffb-2e1d-0410-98e4-8ec43c5233c4

---
+++ com.ibm.wala.cast.java.test.data/src/p/NonPrimaryTopLevel.java
@@ -0,0 +1,11 @@
+package p;
+
+public class NonPrimaryTopLevel {
+    public static void main(String[] args) {
+	NonPrimaryTopLevel nptl= new NonPrimaryTopLevel();
+	Foo f = new Foo();
+    }
+}
+class Foo {
+    
+}
