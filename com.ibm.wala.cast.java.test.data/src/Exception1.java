2b2cb065cb464f186eae32574c8c5512f5b8446d
dolby-oss <dolby-oss@f5eafffb-2e1d-0410-98e4-8ec43c5233c4>
2009-01-09 14:13:17 +0000

    add copyright messages
    git-svn-id: https://wala.svn.sourceforge.net/svnroot/wala/trunk@3169 f5eafffb-2e1d-0410-98e4-8ec43c5233c4

--- com.ibm.wala.cast.java.test.data/src/Exception1.java
+++ com.ibm.wala.cast.java.test.data/src/Exception1.java
@@ -1,3 +1,14 @@
+/******************************************************************************
+ * Copyright (c) 2002 - 2008 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *****************************************************************************/
+
 public class Exception1 {
     public static void main(String[] args) {
 	Exception1 e1= new Exception1();
