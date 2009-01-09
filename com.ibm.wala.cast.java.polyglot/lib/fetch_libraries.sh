877fc2efc992225ddee80bc4e354d2c8d3337132
dolby-oss <dolby-oss@f5eafffb-2e1d-0410-98e4-8ec43c5233c4>
2009-01-09 02:57:52 +0000

    refactor polyglot-based java code in its own projects
    git-svn-id: https://wala.svn.sourceforge.net/svnroot/wala/trunk@3166 f5eafffb-2e1d-0410-98e4-8ec43c5233c4

---
+++ com.ibm.wala.cast.java.polyglot/lib/fetch_libraries.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#REAL_NAME=`realpath $0`
+MY_DIR=`pwd`
+
+TMP=$MY_DIR/tmp
+
+if [[ ! -e $MY_DIR/polyglot.jar ]]; then
+  mkdir -p $TMP
+
+  cd $TMP
+
+  wget -O polyglot-2.0.2-src.tar.gz http://www.cs.cornell.edu/Projects/polyglot/src/polyglot-2.0.2-src.tar.gz
+
+  tar xzf polyglot-2.0.2-src.tar.gz
+
+  cd polyglot-2.0.2-src
+
+  ant jar
+
+  cp lib/polyglot.jar $MY_DIR/polyglot.jar
+  cp lib/java_cup.jar $MY_DIR/java_cup.jar
+
+  cd $MY_DIR
+  rm -rf $TMP
+fi
+
