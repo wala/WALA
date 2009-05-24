4b31ac9bd4908a64c799caa5a20d3c7453d5dfae
omertripp <omertripp@f5eafffb-2e1d-0410-98e4-8ec43c5233c4>
2009-05-24 14:05:01 +0000

    MAINT: - moved treatment of SSAAddressOfInstruction into its own interface to avoid client breakages.
    git-svn-id: https://wala.svn.sourceforge.net/svnroot/wala/trunk@3611 f5eafffb-2e1d-0410-98e4-8ec43c5233c4

---
+++ com.ibm.wala.core/src/com/ibm/wala/ssa/IVisitorWithAddresses.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2007 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package com.ibm.wala.ssa;
+
+import com.ibm.wala.ssa.SSAInstruction.IVisitor;
+
+/**
+ * @author omert
+ *
+ * Temporary interface to accomodate the newly added {@link SSAAddressOfInstruction}
+ * instruction. Ultimately, this interface should be merged into {@link IVisitor}.
+ * 
+ *  TODO: Add 'visitAddressOf' to {@link IVisitor}.
+ */
+public interface IVisitorWithAddresses extends IVisitor {
+  void visitAddressOf(SSAAddressOfInstruction instruction);
+}
