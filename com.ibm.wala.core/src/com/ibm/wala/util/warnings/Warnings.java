478d2788101cf3b6823a434b99e530df41fb0787
sjfink <sjfink@f5eafffb-2e1d-0410-98e4-8ec43c5233c4>
2009-05-26 14:29:08 +0000

    delete some dead comments
    git-svn-id: https://wala.svn.sourceforge.net/svnroot/wala/trunk@3613 f5eafffb-2e1d-0410-98e4-8ec43c5233c4

--- com.ibm.wala.core/src/com/ibm/wala/util/warnings/Warnings.java
+++ com.ibm.wala.core/src/com/ibm/wala/util/warnings/Warnings.java
@@ -33,7 +33,6 @@ public class Warnings {
     warnings.clear();
   }
 
-//  private final static int MAX_PATHS = 200;
 
   public static String asString() {
     TreeSet<Warning> T = new TreeSet<Warning>();
@@ -51,81 +50,4 @@ public class Warnings {
   public static Iterator<Warning> iterator() {
     return warnings.iterator();
   }
-
-//  /**
-//   * This method will print paths for severe warnings. NB: This
-//   * implementation is slow.
-//   */
-//  public String toString(CallGraph cg) {
-//    TreeSet<Warning> T = new TreeSet<Warning>();
-//    T.addAll(this);
-//    Iterator<Warning> it = T.iterator();
-//    StringBuffer result = new StringBuffer();
-//    int nPaths = 0;
-//    if (size() == 0) {
-//      return "No warnings.";
-//    }
-//    for (int i = 1; i <= size(); i++) {
-//      result.append(i).append(". ");
-//      Warning w = it.next();
-//      result.append(w);
-//      result.append("\n");
-//      if (w.getLevel() >= Warning.SEVERE && w instanceof MethodWarning) {
-//        final MemberReference m = ((MethodWarning) w).getMethod();
-//        Filter mFilter = new Filter() {
-//          public boolean accepts(Object o) {
-//            CGNode n = (CGNode)o;
-//            return n.getMethod().getReference().equals(m);
-//          }
-//        };
-//        if (nPaths++ < MAX_PATHS) {
-//          BFSPathFinder<CGNode> p = new BFSPathFinder<CGNode>(cg, cg.getFakeRootNode(), mFilter);
-//          List<CGNode> L = p.find();
-//          if (L == null) {
-//            result.append("   No path found\n");
-//          } else {
-//            result.append("   Path:\n");
-//            for (Iterator<CGNode> it2 = L.iterator(); it2.hasNext();) {
-//              result.append("      ").append(it2.next());
-//              result.append("\n");
-//            }
-//          }
-//        }
-//      }
-//    }
-//    return result.toString();
-//  }
-//
-//  /* a copy of the method above; fix this! */
-//  public void dump(CallGraph cg, PrintStream out) {
-//    TreeSet<Warning> T = new TreeSet<Warning>();
-//    T.addAll(this);
-//    Iterator<Warning> it = T.iterator();
-//    for (int i = 1; i <= size(); i++) {
-//      out.print(i);
-//      out.print(". ");
-//      Warning w = it.next();
-//      out.println(w);
-//      if (w.getLevel() >= Warning.SEVERE && w instanceof MethodWarning) {
-//        final MemberReference m = ((MethodWarning) w).getMethod();
-//        Filter mFilter = new Filter() {
-//          public boolean accepts(Object o) {
-//            CGNode n = (CGNode)o;
-//            return n.getMethod().getReference().equals(m);
-//          }
-//        };
-//        BFSPathFinder<CGNode> p = new BFSPathFinder<CGNode>(cg, cg.getFakeRootNode(), mFilter);
-//        List<CGNode> L = p.find();
-//        if (L == null) {
-//          out.println("   No path found");
-//        } else {
-//          out.println("   Path:");
-//          for (Iterator<CGNode> it2 = L.iterator(); it2.hasNext();) {
-//            out.print("      ");
-//            out.println(it2.next());
-//          }
-//        }
-//      }
-//    }
-//  }
 }
