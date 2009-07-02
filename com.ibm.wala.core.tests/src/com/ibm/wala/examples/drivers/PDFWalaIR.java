bf740fb945839533b3a39f43e05c409b3152a771
sjfink <sjfink@f5eafffb-2e1d-0410-98e4-8ec43c5233c4>
2009-07-02 18:19:24 +0000

    add some comments
    git-svn-id: https://wala.svn.sourceforge.net/svnroot/wala/trunk@3713 f5eafffb-2e1d-0410-98e4-8ec43c5233c4

--- com.ibm.wala.core.tests/src/com/ibm/wala/examples/drivers/PDFWalaIR.java
+++ com.ibm.wala.core.tests/src/com/ibm/wala/examples/drivers/PDFWalaIR.java
@@ -34,35 +34,25 @@ import com.ibm.wala.util.warnings.WalaException;
 import com.ibm.wala.viz.PDFViewUtil;
 
 /**
- * 
- * This simple example application builds a WALA IR and fires off ghostview to
- * visualize a DOT representation.
- * 
- * @author sfink
+ * This simple example application builds a WALA IR and fires off a PDF viewer to visualize a DOT representation.
  */
 public class PDFWalaIR {
 
   final public static String PDF_FILE = "ir.pdf";
 
   /**
-   * Usage: GVWalaIR -appJar [jar file name] -sig [method signature] The "jar
-   * file name" should be something like "c:/temp/testdata/java_cup.jar" The
-   * signature should be something like "java_cup.lexer.advance()V"
-   * 
-   * @param args
-   * @throws IOException 
+   * Usage: PDFWalaIR -appJar [jar file name] -sig [method signature] The "jar file
+   * name" should be something like "c:/temp/testdata/java_cup.jar" The signature should be something like
+   * "java_cup.lexer.advance()V"
    */
   public static void main(String[] args) throws IOException {
     run(args);
   }
 
   /**
-   * @param args
-   *            -appJar [jar file name] -sig [method signature] The "jar file
-   *            name" should be something like "c:/temp/testdata/java_cup.jar"
-   *            The signature should be something like
-   *            "java_cup.lexer.advance()V"
-   * @throws IOException 
+   * @param args -appJar [jar file name] -sig [method signature] The "jar file
+   *          name" should be something like "c:/temp/testdata/java_cup.jar" The signature should be something like
+   *          "java_cup.lexer.advance()V"
    */
   public static Process run(String[] args) throws IOException {
     validateCommandLine(args);
@@ -70,30 +60,43 @@ public class PDFWalaIR {
   }
 
   /**
-   * @param appJar
-   *            should be something like "c:/temp/testdata/java_cup.jar"
-   * @param methodSig
-   *            should be something like "java_cup.lexer.advance()V"
-   * @throws IOException 
+   * @param appJar should be something like "c:/temp/testdata/java_cup.jar"
+   * @param methodSig should be something like "java_cup.lexer.advance()V"
+   * @throws IOException
    */
   public static Process run(String appJar, String methodSig) throws IOException {
     try {
       if (PDFCallGraph.isDirectory(appJar)) {
         appJar = PDFCallGraph.findJarFiles(new String[] { appJar });
       }
-      AnalysisScope scope = AnalysisScopeReader.makeJavaBinaryAnalysisScope(appJar, FileProvider.getFile(CallGraphTestUtil.REGRESSION_EXCLUSIONS));
+      
+      // Build an AnalysisScope which represents the set of classes to analyze.  In particular,
+      // we will analyze the contents of the appJar jar file and the Java standard libraries.
+      AnalysisScope scope = AnalysisScopeReader.makeJavaBinaryAnalysisScope(appJar, FileProvider
+          .getFile(CallGraphTestUtil.REGRESSION_EXCLUSIONS));
 
+      // Build a class hierarchy representing all classes to analyze.  This step will read the class
+      // files and organize them into a tree.
       ClassHierarchy cha = ClassHierarchy.make(scope);
 
+      // Create a name representing the method whose IR we will visualize
       MethodReference mr = StringStuff.makeMethodReference(methodSig);
 
+      // Resolve the method name into the IMethod, the canonical representation of the method information.
       IMethod m = cha.resolveMethod(mr);
       if (m == null) {
         Assertions.UNREACHABLE("could not resolve " + mr);
       }
+      
+      // Set up options which govern analysis choices.  In particular, we will use all Pi nodes when
+      // building the IR.
       AnalysisOptions options = new AnalysisOptions();
       options.getSSAOptions().setPiNodePolicy(SSAOptions.getAllBuiltInPiNodes());
+      
+      // Create an object which caches IRs and related information, reconstructing them lazily on demand.
       AnalysisCache cache = new AnalysisCache();
+      
+      // Build the IR and cache it.
       IR ir = cache.getSSACache().findOrCreateIR(m, Everywhere.EVERYWHERE, options.getSSAOptions());
 
       if (ir == null) {
@@ -129,14 +132,13 @@ public class PDFWalaIR {
    * 
    * Usage:
    * <ul>
-   * <li> args[0] : "-appJar"
-   * <li> args[1] : something like "c:/temp/testdata/java_cup.jar"
-   * <li> args[2] : "-sig"
+   * <li>args[0] : "-appJar"
+   * <li>args[1] : something like "c:/temp/testdata/java_cup.jar"
+   * <li>args[2] : "-sig"
    * <li> args[3] : a method signature like "java_cup.lexer.advance()V" </ul?
    * 
    * @param args
-   * @throws UnsupportedOperationException
-   *             if command-line is malformed.
+   * @throws UnsupportedOperationException if command-line is malformed.
    */
   public static void validateCommandLine(String[] args) {
     if (args.length != 4) {
