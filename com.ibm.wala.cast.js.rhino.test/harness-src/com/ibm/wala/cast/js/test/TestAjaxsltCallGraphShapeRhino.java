063d23adceaefb566de1040a64d16583eb916a54
dolby-oss <dolby-oss@f5eafffb-2e1d-0410-98e4-8ec43c5233c4>
2009-07-16 18:00:08 +0000

    rhino-specific portions of java script analysis tests
    git-svn-id: https://wala.svn.sourceforge.net/svnroot/wala/trunk@3729 f5eafffb-2e1d-0410-98e4-8ec43c5233c4

---
+++ com.ibm.wala.cast.js.rhino.test/harness-src/com/ibm/wala/cast/js/test/TestAjaxsltCallGraphShapeRhino.java
@@ -0,0 +1,32 @@
+/******************************************************************************
+ * Copyright (c) 2002 - 2006 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *****************************************************************************/
+package com.ibm.wala.cast.js.test;
+
+import java.io.IOException;
+import java.net.URL;
+
+import com.ibm.wala.cast.js.translator.CAstRhinoTranslatorFactory;
+import com.ibm.wala.cast.js.translator.JavaScriptTranslatorFactory;
+import com.ibm.wala.ipa.callgraph.CallGraph;
+import com.ibm.wala.util.CancelException;
+
+public class TestAjaxsltCallGraphShapeRhino extends TestJSCallGraphShape {
+
+  public static void main(String[] args) {
+    justThisTest(TestAjaxsltCallGraphShapeRhino.class);
+  }
+
+  public void setUp() {
+    Util.setTranslatorFactory(new CAstRhinoTranslatorFactory());
+  }
+
+
+}
