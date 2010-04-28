8b693ef246d537c43628f5bb4cf0cf424a2067aa
msridhar1 <msridhar1@f5eafffb-2e1d-0410-98e4-8ec43c5233c4>
2010-04-28 15:30:11 +0000

    add a method for getting a method's IR directly; useful for clients that don't build a call graph
    git-svn-id: https://wala.svn.sourceforge.net/svnroot/wala/trunk@3813 f5eafffb-2e1d-0410-98e4-8ec43c5233c4

--- com.ibm.wala.core/src/com/ibm/wala/ipa/callgraph/AnalysisCache.java
+++ com.ibm.wala.core/src/com/ibm/wala/ipa/callgraph/AnalysisCache.java
@@ -11,9 +11,12 @@
 package com.ibm.wala.ipa.callgraph;
 
 import com.ibm.wala.classLoader.IMethod;
+import com.ibm.wala.ipa.callgraph.impl.Everywhere;
 import com.ibm.wala.ssa.DefaultIRFactory;
+import com.ibm.wala.ssa.IR;
 import com.ibm.wala.ssa.IRFactory;
 import com.ibm.wala.ssa.SSACache;
+import com.ibm.wala.ssa.SSAOptions;
 import com.ibm.wala.util.ref.ReferenceCleanser;
 
 /**
@@ -47,5 +50,15 @@ public class AnalysisCache {
 
   public IRFactory<IMethod> getIRFactory() {
     return irFactory;
+  }
+
+  /**
+   * Find or create an IR for the method using the {@link Everywhere} context and default {@link SSAOptions}
+   */
+  public IR getIR(IMethod method) {
+    if (method == null) {
+      throw new IllegalArgumentException("method is null");
+    }
+    return ssaCache.findOrCreateIR(method, Everywhere.EVERYWHERE, new AnalysisOptions().getSSAOptions());
   }
 }
