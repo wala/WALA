e60b23edefe90ed38f9caf4837d12df7466a59de
msridhar1 <msridhar1@f5eafffb-2e1d-0410-98e4-8ec43c5233c4>
2011-07-04 22:14:07 +0000

    reduced the test case
    git-svn-id: https://wala.svn.sourceforge.net/svnroot/wala/trunk@4212 f5eafffb-2e1d-0410-98e4-8ec43c5233c4

--- com.ibm.wala.cast.js.test/examples-src/tests/extjs_switch.js
+++ com.ibm.wala.cast.js.test/examples-src/tests/extjs_switch.js
@@ -1,35 +1,8 @@
-function(date, interval, value) {
-        var d = Ext.Date.clone(date),
-            Date = Ext.Date;
-        if (!interval || value === 0) return d;
 
-        switch(interval.toLowerCase()) {
-            case Ext.Date.MILLI:
-                d.setMilliseconds(d.getMilliseconds() + value);
-                break;
-            case Ext.Date.SECOND:
-                d.setSeconds(d.getSeconds() + value);
-                break;
-            case Ext.Date.MINUTE:
-                d.setMinutes(d.getMinutes() + value);
-                break;
-            case Ext.Date.HOUR:
-                d.setHours(d.getHours() + value);
-                break;
-            case Ext.Date.DAY:
-                d.setDate(d.getDate() + value);
-                break;
-            case Ext.Date.MONTH:
-                var day = date.getDate();
-                if (day > 28) {
-                    day = Math.min(day, Ext.Date.getLastDateOfMonth(Ext.Date.add(Ext.Date.getFirstDateOfMonth(date), 'mo', value)).getDate());
-                }
-                d.setDate(day);
-                d.setMonth(date.getMonth() + value);
-                break;
-            case Ext.Date.YEAR:
-                d.setFullYear(date.getFullYear() + value);
-                break;
+
+function() {
+        switch(0) {
+            case Ext.Date:
         }
-        return d;
     }
+
