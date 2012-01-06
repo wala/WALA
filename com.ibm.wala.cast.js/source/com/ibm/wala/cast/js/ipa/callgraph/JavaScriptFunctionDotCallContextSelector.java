7026301ed4562ab8be3adbaf96e072df598f4bc2
msridhar1 <msridhar1@f5eafffb-2e1d-0410-98e4-8ec43c5233c4>
2012-01-06 21:33:48 +0000

    Fixed unsoundness in the context selector; unsurprisingly, this makes the analysis explode on MooTools again...
    git-svn-id: https://wala.svn.sourceforge.net/svnroot/wala/trunk@4363 f5eafffb-2e1d-0410-98e4-8ec43c5233c4

--- com.ibm.wala.cast.js/source/com/ibm/wala/cast/js/ipa/callgraph/JavaScriptFunctionDotCallContextSelector.java
+++ com.ibm.wala.cast.js/source/com/ibm/wala/cast/js/ipa/callgraph/JavaScriptFunctionDotCallContextSelector.java
@@ -4,7 +4,10 @@ import com.ibm.wala.classLoader.CallSiteReference;
 import com.ibm.wala.classLoader.IMethod;
 import com.ibm.wala.ipa.callgraph.CGNode;
 import com.ibm.wala.ipa.callgraph.Context;
+import com.ibm.wala.ipa.callgraph.ContextItem;
+import com.ibm.wala.ipa.callgraph.ContextKey;
 import com.ibm.wala.ipa.callgraph.ContextSelector;
+import com.ibm.wala.ipa.callgraph.propagation.FilteredPointerKey;
 import com.ibm.wala.ipa.callgraph.propagation.InstanceKey;
 import com.ibm.wala.util.intset.IntSet;
 import com.ibm.wala.util.intset.IntSetUtil;
@@ -26,8 +29,8 @@ public class JavaScriptFunctionDotCallContextSelector implements ContextSelector
     this.base = base;
   }
 
-  // parameter 0 is the function being invoked, parameter 1 its receiver
-  private final static int PARAMETER_INDEX = 1;
+  // parameter 0 is the call function itself, parameter 1 is the function to call, parameter 2 its receiver
+  private final static int PARAMETER_INDEX = 2;
   
   public IntSet getRelevantParameters(CGNode caller, CallSiteReference site) {
     if (caller.getIR().getCalls(site)[0].getNumberOfUses() > PARAMETER_INDEX) {
@@ -42,10 +45,33 @@ public class JavaScriptFunctionDotCallContextSelector implements ContextSelector
     Context baseContext = base.getCalleeTarget(caller, site, callee, receiver);
     // TODO: use a better test here
     if (calleeName.contains(JavaScriptFunctionDotCallTargetSelector.SYNTHETIC_CALL_METHOD_PREFIX)) {
-      if (receiver.length > PARAMETER_INDEX && receiver[PARAMETER_INDEX] != null)
-        // TODO: don't use ForInContext here, introduce new kind of context for specialising on parameter position
-        return new ForInContextSelector.ForInContext(base.getCalleeTarget(caller, site, callee, receiver), receiver[PARAMETER_INDEX]);
+      if (receiver.length > PARAMETER_INDEX && receiver[PARAMETER_INDEX] != null) {
+        return new ParameterInstanceContext(baseContext, PARAMETER_INDEX, receiver[PARAMETER_INDEX]);
+      }
     }
     return baseContext;
+  }
+  
+  public class ParameterInstanceContext implements Context {
+    private final Context baseContext;
+    private final int index;
+    private final InstanceKey key;
+
+    public ParameterInstanceContext(Context baseContext, int index, InstanceKey key) {
+      if(key == null)
+        throw new IllegalArgumentException("Null key provided for parameter.");
+      if(index <= 0 || index > ContextKey.PARAMETERS.length)
+        throw new IllegalArgumentException("Parameter index out of range.");
+      this.baseContext = baseContext;
+      this.index = index;
+      this.key = key;
+    }
+
+    public ContextItem get(ContextKey name) {
+      if(name == ContextKey.PARAMETERS[index])
+        return new FilteredPointerKey.SingleInstanceFilter(key);
+      else
+        return baseContext.get(name);
+    }
   }
 }
