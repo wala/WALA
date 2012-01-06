6e4db282b606de64c0dd4349b4a58b9f1bb85285
msridhar1 <msridhar1@f5eafffb-2e1d-0410-98e4-8ec43c5233c4>
2012-01-06 21:30:59 +0000

    undo change
    git-svn-id: https://wala.svn.sourceforge.net/svnroot/wala/trunk@4351 f5eafffb-2e1d-0410-98e4-8ec43c5233c4

--- com.ibm.wala.cast.js/source/com/ibm/wala/cast/js/ipa/callgraph/JSZeroOrOneXCFABuilder.java
+++ com.ibm.wala.cast.js/source/com/ibm/wala/cast/js/ipa/callgraph/JSZeroOrOneXCFABuilder.java
@@ -30,7 +30,7 @@ import com.ibm.wala.ipa.cha.IClassHierarchy;
  */
 public class JSZeroOrOneXCFABuilder extends JSCFABuilder {
 
-  private static final boolean HANDLE_FUNCTION_APPLY = false;
+  private static final boolean HANDLE_FUNCTION_APPLY = true;
 
   public JSZeroOrOneXCFABuilder(IClassHierarchy cha, AnalysisOptions options, AnalysisCache cache,
       ContextSelector appContextSelector, SSAContextInterpreter appContextInterpreter, int instancePolicy, boolean doOneCFA) {
