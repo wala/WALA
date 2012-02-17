21e17b0928fe9cab280456033cabe6897fc45a4f
msridhar1 <msridhar1@f5eafffb-2e1d-0410-98e4-8ec43c5233c4>
2012-02-17 20:17:30 +0000

    some cleanup
    git-svn-id: https://wala.svn.sourceforge.net/svnroot/wala/trunk@4475 f5eafffb-2e1d-0410-98e4-8ec43c5233c4

--- com.ibm.wala.cast.js/source/com/ibm/wala/cast/js/ipa/callgraph/JavaScriptConstructorContextSelector.java
+++ com.ibm.wala.cast.js/source/com/ibm/wala/cast/js/ipa/callgraph/JavaScriptConstructorContextSelector.java
@@ -10,43 +10,50 @@ import com.ibm.wala.ipa.callgraph.CGNode;
 import com.ibm.wala.ipa.callgraph.Context;
 import com.ibm.wala.ipa.callgraph.ContextSelector;
 import com.ibm.wala.ipa.callgraph.DelegatingContext;
+import com.ibm.wala.ipa.callgraph.impl.Everywhere;
 import com.ibm.wala.ipa.callgraph.propagation.InstanceKey;
 import com.ibm.wala.ipa.callgraph.propagation.cfa.CallerSiteContext;
+import com.ibm.wala.ipa.callgraph.propagation.cfa.OneLevelSiteContextSelector;
 import com.ibm.wala.ipa.callgraph.propagation.cfa.nCFAContextSelector;
 import com.ibm.wala.util.intset.IntSet;
 
 public class JavaScriptConstructorContextSelector implements ContextSelector {
   private final ContextSelector base;
-  
-  private final ContextSelector oneLevel;
+
+  /**
+   * for generating contexts with one-level of call strings, to match standard
+   * Andersen's heap abstraction
+   */
+  private final nCFAContextSelector oneLevelCallStrings;
+
+  private final OneLevelSiteContextSelector oneLevelCallerSite;
   
   public JavaScriptConstructorContextSelector(ContextSelector base) {
     this.base = base;
-    this.oneLevel = new nCFAContextSelector(1, base);
+    this.oneLevelCallStrings = new nCFAContextSelector(1, base);
+    this.oneLevelCallerSite = new OneLevelSiteContextSelector(base);
   }
-  
-  
+
   public IntSet getRelevantParameters(CGNode caller, CallSiteReference site) {
     return base.getRelevantParameters(caller, site);
   }
 
-
-  public Context getCalleeTarget(final CGNode caller, CallSiteReference site, IMethod callee, InstanceKey[] receiver) {   
-    final Context baseCtxt = base.getCalleeTarget(caller, site, callee, receiver);
+  public Context getCalleeTarget(final CGNode caller, CallSiteReference site, IMethod callee, InstanceKey[] receiver) {
     if (callee instanceof JavaScriptConstructor) {
-      final Context oneLevelContext = oneLevel.getCalleeTarget(caller, site, callee, receiver);
+      final Context oneLevelCallStringContext = oneLevelCallStrings.getCalleeTarget(caller, site, callee, receiver);
       final Context callerContext = caller.getContext();
       if (!AstTranslator.NEW_LEXICAL && callerContext instanceof ScopeMappingContext) {
-        return new DelegatingContext(callerContext, new DelegatingContext(oneLevelContext, baseCtxt));
+        return new DelegatingContext(callerContext, oneLevelCallStringContext);
       } else if (AstTranslator.NEW_LEXICAL && LexicalScopingResolverContexts.hasExposedUses(caller, site)) {
-        // use a caller-site context, to enable lexical scoping lookups
-        return new DelegatingContext(new CallerSiteContext(caller, site), baseCtxt);
+        // use a caller-site context, to enable lexical scoping lookups (via caller CGNode)
+        return oneLevelCallerSite.getCalleeTarget(caller, site, callee, receiver);
       } else {
-        // use at least one-level of call-string sensitivity for constructors always
-        return oneLevelContext;
+        // use at least one-level of call-string sensitivity for constructors
+        // always
+        return oneLevelCallStringContext;
       }
     } else {
-      return baseCtxt;
+      return base.getCalleeTarget(caller, site, callee, receiver);
     }
   }
 
