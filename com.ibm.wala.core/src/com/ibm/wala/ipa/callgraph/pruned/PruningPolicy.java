7f7684b57ed3af74d9f63eb92ca768c98bb23cb1
Martin Mohr <martin.mohr@kit.edu>
2014-02-25 16:56:58 +0100

    remove IBM header from class contributed by JOANA project

--- com.ibm.wala.core/src/com/ibm/wala/ipa/callgraph/pruned/PruningPolicy.java
+++ com.ibm.wala.core/src/com/ibm/wala/ipa/callgraph/pruned/PruningPolicy.java
@@ -1,13 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 2007 IBM Corporation.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *     IBM Corporation - initial API and implementation
- *******************************************************************************/
 package com.ibm.wala.ipa.callgraph.pruned;
 
 import com.ibm.wala.ipa.callgraph.CGNode;
