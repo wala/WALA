441cc6f279e13ff745d97e7ce236398ac447cc86
Martin Mohr <martin.mohr@kit.edu>
2015-02-13 17:43:02 +0100

    bugfix in model: close inner loop using a *conditional* goto
    This way, the rest of the outer loop (including the jump to the
    beginning) is also reached in the control-flow which means that
    the outer loop is modelled as a proper loop

--- com.ibm.wala.dalvik/src/com/ibm/wala/dalvik/ipa/callgraph/androidModel/structure/SingleStartAndroidModel.java
+++ com.ibm.wala.dalvik/src/com/ibm/wala/dalvik/ipa/callgraph/androidModel/structure/SingleStartAndroidModel.java
@@ -42,9 +42,12 @@ import org.slf4j.LoggerFactory;
 import com.ibm.wala.dalvik.ipa.callgraph.impl.AndroidEntryPoint.ExecutionOrder;
 import com.ibm.wala.ipa.callgraph.Entrypoint;
 import com.ibm.wala.ipa.summaries.VolatileMethodSummary;
+import com.ibm.wala.shrikeBT.IConditionalBranchInstruction;
+import com.ibm.wala.ssa.ConstantValue;
 import com.ibm.wala.ssa.SSAPhiInstruction;
 import com.ibm.wala.types.TypeReference;
 import com.ibm.wala.util.ssa.SSAValue;
+import com.ibm.wala.util.ssa.SSAValue.NamedKey;
 import com.ibm.wala.util.ssa.SSAValue.TypeKey;
 import com.ibm.wala.util.ssa.SSAValue.VariableKey;
 import com.ibm.wala.util.ssa.SSAValueManager;
@@ -149,7 +152,11 @@ public class SingleStartAndroidModel extends AbstractAndroidModel {
         logger.info("Closing Loop");
         logger.info("PC {}: Goto {}", PC, outerLoopPC);
         if (PC != outerLoopPC) {
-            body.addStatement(insts.GotoInstruction(PC, outerLoopPC));
+            NamedKey trueKey = new SSAValue.NamedKey(TypeReference.BooleanName, "true");
+            SSAValue trueVal = paramManager.getFree(TypeReference.Boolean, trueKey);
+            paramManager.setPhi(trueVal, null);
+            body.addConstant(trueVal.getNumber(), new ConstantValue(true));
+            body.addStatement(insts.ConditionalBranchInstruction(PC, IConditionalBranchInstruction.Operator.EQ, TypeReference.Boolean, trueVal.getNumber(), trueVal.getNumber(), outerLoopPC));
         }
         paramManager.scopeUp();
         
