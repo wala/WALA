cc38735551400a6b0257fab9c615a4fade58d858
Julian Dolby <dolby@us.ibm.com>
2015-02-28 21:55:03 -0500

    break up droid bench

---
+++ com.ibm.wala.dalvik.test/source/com/ibm/wala/dalvik/test/callGraph/droidbench/AliasingTest.java
@@ -0,0 +1,23 @@
+package com.ibm.wala.dalvik.test.callGraph.droidbench;
+
+import java.util.Collection;
+import java.util.Set;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import com.ibm.wala.dalvik.test.callGraph.DroidBenchCGTest;
+import com.ibm.wala.types.MethodReference;
+
+	@RunWith(Parameterized.class)
+	public class AliasingTest extends DroidBenchCGTest {
+    public AliasingTest(String apkFile, Set<MethodReference> uncalled) {
+      super(apkFile, uncalled);
+    }
+
+    @Parameters //(name="DroidBench: {0}")
+    public static Collection<Object[]> generateData() {
+      return DroidBenchCGTest.generateData("Aliasing");
+    }
+	}
