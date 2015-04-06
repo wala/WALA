7d2e5c0731b0a0ca8281d26557a02648204dc903
Julian Dolby <dolby@us.ibm.com>
2015-04-06 11:31:08 -0400

    back to 4.4

--- com.ibm.wala.dalvik.test/source/com/ibm/wala/dalvik/test/callGraph/droidbench/LifecycleTest.java
+++ com.ibm.wala.dalvik.test/source/com/ibm/wala/dalvik/test/callGraph/droidbench/LifecycleTest.java
@@ -20,7 +20,7 @@ public class LifecycleTest extends DroidBenchCGTest {
     super(androidLibs, androidJavaJar, apkFile, uncalled);
   }
 
-  @Parameters // (name="DroidBench: {2}")
+  @Parameters(name="DroidBench: {2}")
   public static Collection<Object[]> generateData() throws IOException {
     return DroidBenchCGTest.generateData(null, androidJavaLib(), "Lifecycle");
   }
