90a98521a1480b9a1eb3ce2993ac4200253f7e86
Julian Dolby <dolby@us.ibm.com>
2015-06-04 17:50:24 -0400

    whitespace

--- com.ibm.wala.cast.java.test.data/src/ArrayLiteral1.java
+++ com.ibm.wala.cast.java.test.data/src/ArrayLiteral1.java
@@ -10,9 +10,8 @@
  *****************************************************************************/
 public class ArrayLiteral1 {
     public static void main(String[] args) {
-	ArrayLiteral1 al1= new ArrayLiteral1();
-	int[] a= new int[] { 0, 1, 2, 3, 5 };
-	Object[] b= new Object[] { null, "hi", new Integer(55), new int[] { 3, 1, 4 } };
-
+      ArrayLiteral1 al1= new ArrayLiteral1();
+      int[] a= new int[] { 0, 1, 2, 3, 5 };
+      Object[] b= new Object[] { null, "hi", new Integer(55), new int[] { 3, 1, 4 } };
     }
 }
