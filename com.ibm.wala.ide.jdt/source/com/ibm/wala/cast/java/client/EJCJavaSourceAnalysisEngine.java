556d8ff0430ebb2ff9591540e6f14c78458c7700
Julian Dolby <dolby@us.ibm.com>
2015-10-14 13:41:23 -0400

    Eclipse Java Compiler standalone for WALA source front end

---
+++ com.ibm.wala.ide.jdt/source/com/ibm/wala/cast/java/client/EJCJavaSourceAnalysisEngine.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2013 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package com.ibm.wala.cast.java.client;
+
+import com.ibm.wala.cast.java.translator.jdt.ejc.EJCClassLoaderFactory;
+import com.ibm.wala.classLoader.ClassLoaderFactory;
+import com.ibm.wala.util.config.SetOfClasses;
+
+public class EJCJavaSourceAnalysisEngine extends JavaSourceAnalysisEngine {
+
+  @Override
+  protected ClassLoaderFactory getClassLoaderFactory(SetOfClasses exclusions) {
+    return new EJCClassLoaderFactory(exclusions);
+  }
+
+}
