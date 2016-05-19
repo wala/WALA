217644ff84477664794fda14092b27e7ab996fd8
Manu Sridharan <m.sridharan@samsung.com>
2016-05-18 21:10:59 -0700

    Example driver for ECJ front end
    This shows how with ECJ, the JDT source front end can be used from
    the command-line (with no running Eclipse instance).

---
+++ com.ibm.wala.ide.jdt/source/com/ibm/wala/ide/util/SourceDirCallGraph.java
@@ -0,0 +1,81 @@
+package com.ibm.wala.ide.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.jar.JarFile;
+
+import com.ibm.wala.cast.ir.ssa.AstIRFactory;
+import com.ibm.wala.cast.java.client.impl.ZeroOneContainerCFABuilderFactory;
+import com.ibm.wala.cast.java.ipa.callgraph.JavaSourceAnalysisScope;
+import com.ibm.wala.cast.java.translator.jdt.ejc.EJCClassLoaderFactory;
+import com.ibm.wala.classLoader.SourceDirectoryTreeModule;
+import com.ibm.wala.ipa.callgraph.AnalysisCache;
+import com.ibm.wala.ipa.callgraph.AnalysisOptions;
+import com.ibm.wala.ipa.callgraph.AnalysisScope;
+import com.ibm.wala.ipa.callgraph.CallGraph;
+import com.ibm.wala.ipa.callgraph.CallGraphBuilder;
+import com.ibm.wala.ipa.callgraph.CallGraphBuilderCancelException;
+import com.ibm.wala.ipa.callgraph.CallGraphStats;
+import com.ibm.wala.ipa.callgraph.Entrypoint;
+import com.ibm.wala.ipa.callgraph.impl.Util;
+import com.ibm.wala.ipa.cha.ClassHierarchy;
+import com.ibm.wala.ipa.cha.ClassHierarchyException;
+import com.ibm.wala.ipa.cha.IClassHierarchy;
+import com.ibm.wala.properties.WalaProperties;
+import com.ibm.wala.types.ClassLoaderReference;
+import com.ibm.wala.util.io.CommandLine;
+import com.ibm.wala.util.warnings.Warnings;
+
+/**
+ * Driver that constructs a call graph for an application specified as a directory of source code.
+ * Example of using the JDT front-end based on ECJ.    
+ * Useful for getting some code to copy-paste.    
+ */
+public class SourceDirCallGraph {
+
+  /**
+   * Usage: ScopeFileCallGraph -sourceDir file_path -mainClass class_name
+   * 
+   * If given -mainClass, uses main() method of class_name as entrypoint.  Class
+   * name should start with an 'L'.
+   * 
+   * Example args: -sourceDir /tmp/srcTest -mainClass LFoo
+   * 
+   */
+  public static void main(String[] args) throws ClassHierarchyException, IllegalArgumentException, CallGraphBuilderCancelException, IOException {
+    long start = System.currentTimeMillis();
+    Properties p = CommandLine.parse(args);
+    String sourceDir = p.getProperty("sourceDir");
+    String mainClass = p.getProperty("mainClass");
+    AnalysisScope scope = new JavaSourceAnalysisScope();
+    // add standard libraries to scope
+    String[] stdlibs = WalaProperties.getJ2SEJarFiles();
+    for (int i = 0; i < stdlibs.length; i++) {
+      scope.addToScope(ClassLoaderReference.Primordial, new JarFile(stdlibs[i]));
+    }
+    // add the source directory
+    scope.addToScope(JavaSourceAnalysisScope.SOURCE, new SourceDirectoryTreeModule(new File(sourceDir)));
+    
+    // build the class hierarchy
+    IClassHierarchy cha = ClassHierarchy.make(scope, new EJCClassLoaderFactory(scope.getExclusions()));
+    System.out.println(cha.getNumberOfClasses() + " classes");
+    System.out.println(Warnings.asString());
+    Warnings.clear();
+    AnalysisOptions options = new AnalysisOptions();
+    Iterable<Entrypoint> entrypoints = Util.makeMainEntrypoints(JavaSourceAnalysisScope.SOURCE, cha, new String[] { mainClass } );
+    options.setEntrypoints(entrypoints);
+    // you can dial down reflection handling if you like
+//    options.setReflectionOptions(ReflectionOptions.NONE);
+    AnalysisCache cache = new AnalysisCache(AstIRFactory.makeDefaultFactory());
+    //CallGraphBuilder builder = new ZeroCFABuilderFactory().make(options, cache, cha, scope, false);
+    CallGraphBuilder builder = new ZeroOneContainerCFABuilderFactory().make(options, cache, cha, scope, false);
+    System.out.println("building call graph...");
+    CallGraph cg = builder.makeCallGraph(options, null);
+    long end = System.currentTimeMillis();
+    System.out.println("done");
+    System.out.println("took " + (end-start) + "ms");
+    System.out.println(CallGraphStats.getStats(cg));    
+  }
+
+}
