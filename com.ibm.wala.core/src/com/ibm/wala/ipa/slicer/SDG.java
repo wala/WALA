f42ad7aea67be7e6436def71c05c8e2075750aa7
Manu Sridharan <msridhar@gmail.com>
2017-01-02 14:26:37 -0800

    Fix #127
    Check that we only loop over the parameters actually being passed at
    the call site in the SDG construction.

--- com.ibm.wala.core/src/com/ibm/wala/ipa/slicer/SDG.java
+++ com.ibm.wala.core/src/com/ibm/wala/ipa/slicer/SDG.java
@@ -556,11 +556,15 @@ public class SDG<T extends InstanceKey> extends AbstractNumberedGraph<Statement>
       case PARAM_CALLER: {
         ParamCaller pac = (ParamCaller) N;
         SSAAbstractInvokeInstruction call = pac.getInstruction();
+        int numParamsPassed = call.getNumberOfUses();
         Collection<Statement> result = HashSetFactory.make(5);
         if (!dOptions.equals(DataDependenceOptions.NONE)) {
           // data dependence successors
           for (CGNode t : cg.getPossibleTargets(N.getNode(), call.getCallSite())) {
-            for (int i = 0; i < t.getMethod().getNumberOfParameters(); i++) {
+            // in some languages (*cough* JavaScript *cough*) you can pass
+            // fewer parameters than the number of formals.  So, only loop
+            // over the parameters actually being passed here
+            for (int i = 0; i < t.getMethod().getNumberOfParameters() && i < numParamsPassed; i++) {
               if (dOptions.isTerminateAtCast() && call.isDispatch() && pac.getValueNumber() == call.getReceiver()) {
                 // a virtual dispatch is just like a cast.
                 continue;
