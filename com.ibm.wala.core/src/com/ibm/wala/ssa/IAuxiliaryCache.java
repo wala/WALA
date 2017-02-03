96be29550047014bdd429c70b63c709a0fa0ef20
Julian Dolby <juliandolby@gmail.com>
2017-02-02 20:46:34 -0500

    grr.  commit -a does not find new files.

---
+++ com.ibm.wala.core/src/com/ibm/wala/ssa/IAuxiliaryCache.java
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Copyright (c) 2007 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package com.ibm.wala.ssa;
+
+import com.ibm.wala.classLoader.IMethod;
+import com.ibm.wala.ipa.callgraph.Context;
+
+interface IAuxiliaryCache {
+
+  /**
+   * The existence of this is unfortunate.
+   */
+  void wipe();
+
+  /**
+   * @param m a method
+   * @param options options governing ssa construction
+   * @return the object cached for m, or null if none found
+   */
+  Object find(IMethod m, Context c, SSAOptions options);
+
+  /**
+   * cache new auxiliary information for an <m,options> pair
+   * 
+   * @param m a method
+   * @param options options governing ssa construction
+   */
+  void cache(IMethod m, Context c, SSAOptions options, Object aux);
+
+  /**
+   * invalidate all cached information about a method
+   */
+  void invalidate(IMethod method, Context c);
+
+}
