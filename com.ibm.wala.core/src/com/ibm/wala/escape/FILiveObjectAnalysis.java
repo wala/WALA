wala/WALA

commit fde65340d22f153914343a33bbaf6f5497ce67f4

Fix 132 Eclipse warnings about using raw generic types

com.ibm.wala.core/src/com/ibm/wala/escape/FILiveObjectAnalysis.java
@@ -48,7 +48,7 @@ public class FILiveObjectAnalysis implements ILiveObjectAnalysis {
   /**
    * Graph view of pointer analysis results
    */
-  private final HeapGraph heapGraph;
+  private final HeapGraph<InstanceKey> heapGraph;
 
   /**
    * Cached map from InstanceKey -> Set<CGNode>
@@ -68,7 +68,7 @@ public class FILiveObjectAnalysis implements ILiveObjectAnalysis {
   /**
    * 
    */
-  public FILiveObjectAnalysis(CallGraph callGraph, HeapGraph heapGraph, boolean expensiveIntraproceduralAnalysis) {
+  public FILiveObjectAnalysis(CallGraph callGraph, HeapGraph<InstanceKey> heapGraph, boolean expensiveIntraproceduralAnalysis) {
     super();
     this.callGraph = callGraph;
     this.heapGraph = heapGraph;

commit 2a33cb36497038b3b99b9f8c41288fc47c4554cf

Generalize an over-constrained generic parameter
This fixes WALA bug #151, which was first introduced in commit fde6534.

com.ibm.wala.core/src/com/ibm/wala/escape/FILiveObjectAnalysis.java
@@ -48,7 +48,7 @@ public class FILiveObjectAnalysis implements ILiveObjectAnalysis {
   /**
    * Graph view of pointer analysis results
    */
-  private final HeapGraph<InstanceKey> heapGraph;
+  private final HeapGraph<? super InstanceKey> heapGraph;
 
   /**
    * Cached map from InstanceKey -> Set<CGNode>
@@ -68,7 +68,7 @@ public class FILiveObjectAnalysis implements ILiveObjectAnalysis {
   /**
    * 
    */
-  public FILiveObjectAnalysis(CallGraph callGraph, HeapGraph<InstanceKey> heapGraph, boolean expensiveIntraproceduralAnalysis) {
+  public FILiveObjectAnalysis(CallGraph callGraph, HeapGraph<? super InstanceKey> heapGraph, boolean expensiveIntraproceduralAnalysis) {
     super();
     this.callGraph = callGraph;
     this.heapGraph = heapGraph;