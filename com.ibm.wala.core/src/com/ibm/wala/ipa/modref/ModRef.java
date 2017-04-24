ea49d92cc9546b8bfc5962252293c8093bb8511c
Raffi Khatchadourian <raffi.khatchadourian@hunter.cuny.edu>
2017-04-24 16:26:23 -0400

    Some generic type fixes in ModRef.java.

--- com.ibm.wala.core/src/com/ibm/wala/ipa/modref/ModRef.java
+++ com.ibm.wala.core/src/com/ibm/wala/ipa/modref/ModRef.java
@@ -44,8 +44,8 @@ import com.ibm.wala.util.intset.OrdinalSet;
  */
 public class ModRef<T extends InstanceKey> {
 
-  public static ModRef make() {
-    return new ModRef();
+  public static <T extends InstanceKey> ModRef<T> make() {
+    return new ModRef<T>();
   }
 
   protected ModRef() {
@@ -342,9 +342,9 @@ public class ModRef<T extends InstanceKey> {
     return makeModVisitor(n, result, pa, h, false);
   }
 
-  protected ModVisitor makeModVisitor(CGNode n, Collection<PointerKey> result, PointerAnalysis<T> pa, ExtendedHeapModel h,
+  protected ModVisitor<T,? extends ExtendedHeapModel> makeModVisitor(CGNode n, Collection<PointerKey> result, PointerAnalysis<T> pa, ExtendedHeapModel h,
       boolean ignoreAllocHeapDefs) {
-    return new ModVisitor(n, result, h, pa, ignoreAllocHeapDefs);
+    return new ModVisitor<>(n, result, h, pa, ignoreAllocHeapDefs);
   }
 
   /**
@@ -368,7 +368,7 @@ public class ModRef<T extends InstanceKey> {
     return hexcl == null ? result : hexcl.filter(result);
   }
 
-  protected RefVisitor makeRefVisitor(CGNode n, Collection<PointerKey> result, PointerAnalysis<T> pa, ExtendedHeapModel h) {
+  protected RefVisitor<T, ExtendedHeapModel> makeRefVisitor(CGNode n, Collection<PointerKey> result, PointerAnalysis<T> pa, ExtendedHeapModel h) {
     return new RefVisitor<T, ExtendedHeapModel>(n, result, pa, h);
   }
 
