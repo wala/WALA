9585aaf0a2ff5f85ec1501d50cee1d35552f4062
Manu Sridharan <msridhar@uber.com>
2017-05-24 14:43:00 -0700

    make overriding method public

--- com.ibm.wala.scandroid/source/org/scandroid/util/DexDotUtil.java
+++ com.ibm.wala.scandroid/source/org/scandroid/util/DexDotUtil.java
@@ -176,7 +176,7 @@ public class DexDotUtil extends DotUtil {
        * @return StringBuffer holding dot output representing G
        * @throws WalaException
        */
-      private static <T> StringBuffer dotOutput(Graph<T> g, NodeDecorator<T> labels, String title) throws WalaException {
+      public static <T> StringBuffer dotOutput(Graph<T> g, NodeDecorator<T> labels, String title) throws WalaException {
         StringBuffer result = new StringBuffer("digraph \"DirectedGraph\" {\n");
 
         if (title != null) {
