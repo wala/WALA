00ad57fedec0fe684561c41c0a4cdf3fcff1f80a
Julian Dolby <dolby@us.ibm.com>
2017-07-24 17:28:24 +0000

    trigger WALA-Mobile too

--- dependent-projects-trigger.sh
+++ dependent-projects-trigger.sh
@@ -12,3 +12,11 @@ curl -s -X POST \
    -H "Authorization: token $1" \
    -d "$body" \
    https://api.travis-ci.org/repo/wala%2FClient/requests
+
+curl -s -X POST \
+   -H "Content-Type: application/json" \
+   -H "Accept: application/json" \
+   -H "Travis-API-Version: 3" \
+   -H "Authorization: token $1" \
+   -d "$body" \
+   https://api.travis-ci.org/repo/wala%2FWALA-Mobile/requests
