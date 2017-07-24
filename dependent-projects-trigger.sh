18ea72d1d75977d4b745d4e23d5ed66460100ca4
Julian Dolby <juliandolby@gmail.com>
2017-07-24 10:05:46 -0400

    try to trigger WALA Client

---
+++ dependent-projects-trigger.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+body='{
+"request": {
+"branch":"master"
+}}'
+
+curl -s -X POST \
+   -H "Content-Type: application/json" \
+   -H "Accept: application/json" \
+   -H "Travis-API-Version: 3" \
+   -H "Authorization: token $1" \
+   -d "$body" \
+   https://api.travis-ci.org/repo/wala%2FClient/requests
