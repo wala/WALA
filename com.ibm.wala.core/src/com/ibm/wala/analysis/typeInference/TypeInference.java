db84793cf11dd23a585acc996111effad08c1900
Raffi Khatchadourian <raffi.khatchadourian@hunter.cuny.edu>
2017-08-11 19:24:42 -0400

    Only get the variable once.

--- com.ibm.wala.core/src/com/ibm/wala/analysis/typeInference/TypeInference.java
+++ com.ibm.wala.core/src/com/ibm/wala/analysis/typeInference/TypeInference.java
@@ -752,8 +752,9 @@ public class TypeInference extends SSAInference<TypeVariable> implements FixedPo
     if (valueNumber < 0) {
       throw new IllegalArgumentException("bad value number " + valueNumber);
     }
-    assert getVariable(valueNumber) != null : "null variable for value number " + valueNumber;
-    return getVariable(valueNumber).getType();
+    TypeVariable variable = getVariable(valueNumber);
+    assert variable != null : "null variable for value number " + valueNumber;
+    return variable.getType();
   }
 
   public TypeAbstraction getConstantType(int valueNumber) {
