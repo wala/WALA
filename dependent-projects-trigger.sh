8ebfd7615cad588ece58bb5858d447e16c658933
Julian Dolby <juliandolby@gmail.com>
2017-08-12 20:02:18 -0400

    add MemSAT

--- dependent-projects-trigger.sh
+++ dependent-projects-trigger.sh
@@ -20,3 +20,11 @@ curl -s -X POST \
    -H "Authorization: token $1" \
    -d "$body" \
    https://api.travis-ci.org/repo/wala%2FWALA-Mobile/requests
+
+curl -s -X POST \
+   -H "Content-Type: application/json" \
+   -H "Accept: application/json" \
+   -H "Travis-API-Version: 3" \
+   -H "Authorization: token $1" \
+   -d "$body" \
+   https://api.travis-ci.org/repo/wala%2FMemSAT/requests
