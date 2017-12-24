76faa4df2dfe576a6dd1b145e69d419775c378d9
Manu Sridharan <msridhar@gmail.com>
2017-12-23 16:38:26 -0800

    Fix bug with method lookup (#276)
    The previous lookup logic would erroneously look in superclasses for a constructor with parameters.
    
    Bug reported on [the mailing list](https://groups.google.com/forum/#!msg/wala-sourceforge-net/6m_VmDF-lZ0/erZIYQ68BwAJ;context-place=topic/wala-sourceforge-net/TjRzdDyljjY)

---
+++ com.ibm.wala.core.testdata/src/methodLookup/MethodLookupStuff.java
@@ -0,0 +1,15 @@
+package methodLookup;
+
+public class MethodLookupStuff {
+
+  static class A {
+    A(int foo) {      
+    }
+    
+    A() {}
+  }
+  
+  static class B extends A {
+    
+  }
+}
