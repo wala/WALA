9a3ecd6ab92cf8630311e465f7af5fe65e25a688
Ben Liblit <liblit@cs.wisc.edu>
2018-01-14 22:22:19 -0600

    Disable some Eclipse warnings on Java test inputs
    I think these were previously not being compiled at all.  Now, with
    Buildship generating Eclipse ".project" settings automatically, these
    are being processed.  In general we don't care much about questionable
    code in test data, though.

--- com.ibm.wala.ide.jdt.test/source/com/ibm/wala/demandpa/driver/DemandCastChecker.java
+++ com.ibm.wala.ide.jdt.test/source/com/ibm/wala/demandpa/driver/DemandCastChecker.java
@@ -163,7 +163,7 @@ public class DemandCastChecker {
     return fullDemandPointsTo;
   }
 
-  private static String getExclusions(@SuppressWarnings("unused") String benchName) {
+  private static String getExclusions(String benchName) {
     return CallGraphTestUtil.REGRESSION_EXCLUSIONS;
   }
 
@@ -208,7 +208,6 @@ public class DemandCastChecker {
     return Pair.make(retCG, retPA);
   }
 
-  @SuppressWarnings("unused")
   private static RefinementPolicyFactory chooseRefinePolicyFactory(ClassHierarchy cha) {
     if (true) {
       return new TunedRefinementPolicy.Factory(cha);
