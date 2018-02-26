cac0e870827249903725a26bd2bcc804bd848f19
Ben Liblit <liblit@cs.wisc.edu>
2018-02-26 13:58:12 -0600

    Fix remaining Javadoc warnings
    This fixes the last of our Javadoc warnings without creating a
    circular dependency between ":com.ibm.wala.cast:javadoc" and
    ":com.ibm.wala.cast.js:javadoc".  Fixes #4, wherein more details about
    this tricky dependency challenge can be found.

---
+++ buildSrc/src/main/groovy/package-list.groovy
@@ -0,0 +1,28 @@
+import org.gradle.api.tasks.*
+
+
+////////////////////////////////////////////////////////////////////////
+//
+//  create a Javadoc-style "package-list" file
+//
+
+class CreatePackageList extends org.gradle.api.DefaultTask {
+
+	@Input Object sourceSet
+	@OutputFile File packageList = new File("$temporaryDir/package-list")
+
+	@TaskAction
+	def create() {
+		sourceSet.sourceCollections.collect { collection ->
+			def sourceRoot = collection.tree.dir.toPath()
+			collection.collect { source ->
+				def javaSourceFilePath = source.toPath()
+				def parentPath = javaSourceFilePath.parent
+				def relativePath = sourceRoot.relativize(parentPath)
+				relativePath.toString().replace(File.separator, '.')
+			}
+		}.flatten().sort().unique().each {
+			packageList << "$it\n"
+		}
+	}
+}
