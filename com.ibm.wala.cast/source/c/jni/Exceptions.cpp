e8b86fdf822e69d3d618d4683c6cb939b487dcbd
Ben Liblit <liblit@cs.wisc.edu>
2018-03-19 17:30:38 -0500

    Clean up questionable memory management during string construction
    Avoid allocating memory using strdup() and then releasing it using
    operator delete.  strdup()-allocated memory should be released using
    free(), not delete.  But in these specific cases, there really was
    never any good reason to duplicate the C-style strings in the first
    place.  Instead, we can safely pass those NUL-terminated char pointers
    directly in calls to JNI's NewStringUTF().  NewStringUTF() does not
    take ownership of its argument, but rather clones that string data
    internally before returning.  So using strdup() and delete was just
    unnecessary memory churn.
    
    In cases where we need to format, concatenate, or construct new
    strings, don't use sprintf() into variable-sized, stack-allocated
    arrays.  Variable-sized arrays are not portable, and in particular are
    rejected by Microsoft's Visual Studio 2017 C++ compiler.  Instead, do
    our string formatting and manipulations using std::ostringstream
    and/or std::string.  We just need to be a bit careful about the
    lifetimes and ownership responsibilities of allocated data.  In
    brief, (1) ostringstream::str() returns a temporary string instance
    that expires at the end of the enclosing statement, independent of the
    lifetime of the ostringstream instance; while (2) string::c_str()
    returns an pointer to internal data that remains valid as long as the
    string on which it was called is valid and unmodified.

--- com.ibm.wala.cast/source/c/jni/Exceptions.cpp
+++ com.ibm.wala.cast/source/c/jni/Exceptions.cpp
@@ -1,11 +1,12 @@
+#include <csetjmp>
 #include <jni.h>
-#include <setjmp.h>
-#include <string.h>
+#include <sstream>
+#include <string>
 #include "Exceptions.h"
 
-#ifdef _MSC_VER
-#include <malloc.h>
-#endif
+using std::ostringstream;
+using std::string;
+
 
 Exceptions::Exceptions(JNIEnv *java_env, jmp_buf& c_env) : 
   _java_env(java_env), 
@@ -23,14 +24,18 @@ void Exceptions::throwAnyException(const char *file_name, int line_number) {
   if (_java_env->ExceptionCheck()) throwException(file_name, line_number);
 }
 
+static void formatExceptionMessage(ostringstream &formatter, const char *file_name, int line_number) {
+  formatter << "exception at " << file_name << ':' << line_number;
+}
+
 void Exceptions::throwException(const char *file_name, int line_number) {
   jthrowable real_ex = _java_env->ExceptionOccurred();
   _java_env->ExceptionClear();
 
-  char msg[strlen(file_name) + 1024];
-  memset(msg, 0, strlen(file_name) + 1024);
-  sprintf(msg, "exception at %s:%d", file_name, line_number);
-  jstring java_message = _java_env->NewStringUTF(msg);
+  ostringstream formatter;
+  formatExceptionMessage(formatter, file_name, line_number);
+  string message = formatter.str();
+  jstring java_message = _java_env->NewStringUTF(message.c_str());
 
   jthrowable ex = (jthrowable)
     _java_env->NewObject(_jre, _wrapper_ctr, java_message, real_ex);
@@ -47,10 +52,11 @@ void Exceptions::throwException(const char *file_name, int line_number) {
 
 void 
 Exceptions::throwException(const char *file_name, int line_number, const char *c_message) {
-  char msg[strlen(file_name) + strlen(c_message) + 1024]; 
-  memset(msg, 0, strlen(file_name) + strlen(c_message) + 1024);
-  sprintf(msg, "exception at %s:%d: %s", file_name, line_number, c_message);
-  jstring java_message = _java_env->NewStringUTF(msg);
+  ostringstream formatter;
+  formatExceptionMessage(formatter, file_name, line_number);
+  formatter << ": " << c_message;
+  string message = formatter.str();
+  jstring java_message = _java_env->NewStringUTF(message.c_str());
   jthrowable ex = (jthrowable)_java_env->NewObject(_jre, _ctr, java_message);
   _java_env->Throw(ex);
   
