44986198dde5abf76a2e984830eec3b1b274aa42
Ben Liblit <liblit@cs.wisc.edu>
2019-01-26 21:08:54 -0800

    Avoid curly braces within {@code ...}
    These are rejected by whatever Maven uses to build Javadoc.

--- com.ibm.wala.core/src/com/ibm/wala/analysis/arraybounds/ArrayBoundsGraphBuilder.java
+++ com.ibm.wala.core/src/com/ibm/wala/analysis/arraybounds/ArrayBoundsGraphBuilder.java
@@ -123,7 +123,7 @@ public class ArrayBoundsGraphBuilder {
 	 * Case 2: no restriction is given by the branch (i.e. the operator is not equal)
 	 * {piParent} --&gt; {piVar}
 	 *
-	 * {@code if (piParent op piRestrictor) {piVar = piParent}}
+	 * <pre>if (piParent op piRestrictor) {piVar = piParent}</pre>
 	 */
 	private void addPiStructure(Integer piVar, Integer piParent,
 			Integer piRestrictor, Operator op) {
