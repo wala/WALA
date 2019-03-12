7245f2a75d0dad853d15d8fe2948e1fd15c15878
Ben Liblit <liblit@cs.wisc.edu>
2019-03-11 20:46:50 -0500

    Correct dangling Javadoc for entire package

--- com.ibm.wala.core/src/com/ibm/wala/analysis/arraybounds/hypergraph/package-info.java
+++ com.ibm.wala.core/src/com/ibm/wala/analysis/arraybounds/hypergraph/package-info.java
@@ -1,8 +1,7 @@
-package com.ibm.wala.analysis.arraybounds.hypergraph;
-
 /**
  * This package contains a generic implementation of directed hypergraphs. The implementation is
  * optimized for shortest path search, with {@link
  * com.ibm.wala.analysis.arraybounds.hypergraph.algorithms.ShortestPath#compute(com.ibm.wala.analysis.arraybounds.hypergraph.DirectedHyperGraph,
  * com.ibm.wala.analysis.arraybounds.hypergraph.HyperNode, java.util.Comparator) ShortestPath}
  */
+package com.ibm.wala.analysis.arraybounds.hypergraph;
