5cf6af7b5ab9d46c484f15078cdabca7ad930ffc
Manu Sridharan <msridhar@gmail.com>
2019-05-05 10:07:28 -0700

    Refactor some lambda-related code

--- com.ibm.wala.core/src/com/ibm/wala/ipa/summaries/LambdaMethodTargetSelector.java
+++ com.ibm.wala.core/src/com/ibm/wala/ipa/summaries/LambdaMethodTargetSelector.java
@@ -17,6 +17,7 @@ import com.ibm.wala.classLoader.Language;
 import com.ibm.wala.classLoader.NewSiteReference;
 import com.ibm.wala.ipa.callgraph.CGNode;
 import com.ibm.wala.ipa.callgraph.MethodTargetSelector;
+import com.ibm.wala.ipa.cha.IClassHierarchy;
 import com.ibm.wala.shrikeCT.BootstrapMethodsReader.BootstrapMethod;
 import com.ibm.wala.ssa.SSAInstructionFactory;
 import com.ibm.wala.ssa.SSAInvokeDynamicInstruction;
@@ -25,6 +26,7 @@ import com.ibm.wala.types.TypeReference;
 import com.ibm.wala.util.collections.HashMapFactory;
 import com.ibm.wala.util.strings.Atom;
 import java.util.Map;
+import java.util.Objects;
 
 public class LambdaMethodTargetSelector implements MethodTargetSelector {
 
@@ -40,76 +42,77 @@ public class LambdaMethodTargetSelector implements MethodTargetSelector {
 
   @Override
   public IMethod getCalleeTarget(CGNode caller, CallSiteReference site, IClass receiver) {
-    if (!site.getDeclaredTarget().getName().equals(MethodReference.clinitName)
-        && caller.getClassHierarchy().lookupClass(TypeReference.LambdaMetaFactory) != null
-        && caller
-            .getClassHierarchy()
-            .lookupClass(TypeReference.LambdaMetaFactory)
-            .equals(
-                caller
-                    .getClassHierarchy()
-                    .lookupClass(site.getDeclaredTarget().getDeclaringClass()))) {
+    IClassHierarchy cha = caller.getClassHierarchy();
+    MethodReference target = site.getDeclaredTarget();
+    if (isNonClinitLambdaMetafactoryMethod(cha, target)) {
       SSAInvokeDynamicInstruction invoke =
           (SSAInvokeDynamicInstruction) caller.getIR().getCalls(site)[0];
 
       if (!methodSummaries.containsKey(invoke.getBootstrap())) {
-        String cls =
-            caller
-                .getMethod()
-                .getDeclaringClass()
-                .getName()
-                .toString()
-                .replace("/", "$")
-                .substring(1);
-        int bootstrapIndex = invoke.getBootstrap().getIndexInClassFile();
-        MethodReference ref =
-            MethodReference.findOrCreate(
-                site.getDeclaredTarget().getDeclaringClass(),
-                Atom.findOrCreateUnicodeAtom(
-                    site.getDeclaredTarget().getName().toString()
-                        + '$'
-                        + cls
-                        + '$'
-                        + bootstrapIndex),
-                site.getDeclaredTarget().getDescriptor());
-
-        MethodSummary summary = new MethodSummary(ref);
-
-        if (site.isStatic()) {
-          summary.setStatic(true);
-        }
-
-        int index = 0;
-        int v = site.getDeclaredTarget().getNumberOfParameters() + 2;
-        IClass lambda = getLambdaSummaryClass(caller, invoke);
-        SSAInstructionFactory insts = Language.JAVA.instructionFactory();
-        summary.addStatement(
-            insts.NewInstruction(index, v, NewSiteReference.make(index, lambda.getReference())));
-        index++;
-        for (int i = 0; i < site.getDeclaredTarget().getNumberOfParameters(); i++) {
-          summary.addStatement(
-              insts.PutInstruction(
-                  index++,
-                  v,
-                  i + 1,
-                  lambda.getField(Atom.findOrCreateUnicodeAtom("c" + i)).getReference()));
-        }
-        summary.addStatement(insts.ReturnInstruction(index++, v, false));
+        MethodSummary summary = getSummaryForBootstrapMethod(caller, site, target, invoke);
 
         methodSummaries.put(
             invoke.getBootstrap(),
             new SummarizedMethod(
-                ref,
-                summary,
-                caller
-                    .getClassHierarchy()
-                    .lookupClass(site.getDeclaredTarget().getDeclaringClass())));
+                summary.getMethod(), summary, cha.lookupClass(target.getDeclaringClass())));
       }
       return methodSummaries.get(invoke.getBootstrap());
 
     } else {
       return base.getCalleeTarget(caller, site, receiver);
     }
+  }
+
+  /**
+   * create a summary for a bootstrap method. The generated IR in the summary allocates a
+   * corresponding {@link LambdaSummaryClass} and sets its fields to correspond to the target
+   * parameters.
+   */
+  private MethodSummary getSummaryForBootstrapMethod(
+      CGNode caller,
+      CallSiteReference site,
+      MethodReference target,
+      SSAInvokeDynamicInstruction invoke) {
+    String cls =
+        caller.getMethod().getDeclaringClass().getName().toString().replace("/", "$").substring(1);
+    int bootstrapIndex = invoke.getBootstrap().getIndexInClassFile();
+    MethodReference ref =
+        MethodReference.findOrCreate(
+            target.getDeclaringClass(),
+            Atom.findOrCreateUnicodeAtom(
+                target.getName().toString() + '$' + cls + '$' + bootstrapIndex),
+            target.getDescriptor());
+
+    MethodSummary summary = new MethodSummary(ref);
+
+    if (site.isStatic()) {
+      summary.setStatic(true);
+    }
+
+    int index = 0;
+    int v = target.getNumberOfParameters() + 2;
+    IClass lambda = getLambdaSummaryClass(caller, invoke);
+    SSAInstructionFactory insts = Language.JAVA.instructionFactory();
+    summary.addStatement(
+        insts.NewInstruction(index, v, NewSiteReference.make(index, lambda.getReference())));
+    index++;
+    for (int i = 0; i < target.getNumberOfParameters(); i++) {
+      summary.addStatement(
+          insts.PutInstruction(
+              index++,
+              v,
+              i + 1,
+              lambda.getField(Atom.findOrCreateUnicodeAtom("c" + i)).getReference()));
+    }
+    summary.addStatement(insts.ReturnInstruction(index++, v, false));
+    return summary;
+  }
+
+  private boolean isNonClinitLambdaMetafactoryMethod(IClassHierarchy cha, MethodReference target) {
+    return !target.getName().equals(MethodReference.clinitName)
+        && Objects.equals(
+            cha.lookupClass(TypeReference.LambdaMetaFactory),
+            cha.lookupClass(target.getDeclaringClass()));
   }
 
   private LambdaSummaryClass getLambdaSummaryClass(
