aad9048b19ed5170d002dae97566a8c96f7be9d4
Manu Sridharan <msridhar@gmail.com>
2019-05-11 13:53:26 -0700

    cleanup

--- deploy_snapshot_and_javadoc.sh
+++ deploy_snapshot_and_javadoc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -eux
 #
 # Deploy a jar, source jar, and javadoc jar to Sonatype's snapshot repo.
+# Also deploy javadocs to our web site.
 #
 # Adapted from https://coderwall.com/p/9b_lfq and
 # http://benlimmer.com/2013/12/26/automatically-publish-javadoc-to-gh-pages-with-travis-ci/
@@ -24,4 +25,23 @@ else
   echo "Deploying snapshot..."
   ./gradlew clean uploadArchives
   echo "Snapshot deployed!"
+
+  echo "Uploading javadoc..."
+
+  ./gradlew aggregatedJavadocs
+
+  cd $HOME
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "travis-ci"
+  git clone --quiet https://${GH_TOKEN}@github.com/wala/javadoc > /dev/null
+
+  cd javadoc
+  git rm -rf --quiet *
+  cp -Rf $HOME/build/wala/WALA/build/docs/javadoc/* .
+  git add -f .
+  git commit -m "Latest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+  git push > /dev/null
+
+  echo -e "Published Javadoc to gh-pages.\n"
+  
 fi
