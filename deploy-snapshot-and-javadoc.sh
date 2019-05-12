fbe974bdbc45c94ca443474a64c7aaff677e0144
Manu Sridharan <msridhar@gmail.com>
2019-05-12 16:41:19 -0700

    Do javadoc upload at the same time we upload snapshot builds (#491)
    Moving another part of the build from Maven to Gradle.  This also adds an `aggregatedJavadocs` task (from [here](https://discuss.gradle.org/t/best-approach-gradle-multi-module-project-generate-just-one-global-javadoc/18657/21)) to create the aggregate Javadocs.

--- deploy-snapshot-and-javadoc.sh
+++ deploy-snapshot-and-javadoc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -eux
 #
 # Deploy a jar, source jar, and javadoc jar to Sonatype's snapshot repo.
+# Also deploy javadocs to our web site.
 #
 # Adapted from https://coderwall.com/p/9b_lfq and
 # http://benlimmer.com/2013/12/26/automatically-publish-javadoc-to-gh-pages-with-travis-ci/
@@ -24,4 +25,29 @@ else
   echo "Deploying snapshot..."
   ./gradlew clean uploadArchives
   echo "Snapshot deployed!"
+
+  echo "Uploading javadoc..."
+
+  ./gradlew aggregatedJavadocs
+
+  (
+  cd build/docs/javadoc
+  git init
+  git add .
+  git commit -m "Latest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+
+  {
+    set +x
+    git config --global user.email "travis@travis-ci.org"
+    git config --global user.name "travis-ci"
+    git remote add origin "https://${GH_TOKEN}@github.com/wala/javadoc"
+    set -x
+  }
+
+  # we can force push here since we don't care about maintaining javadoc history for every commit
+  git push origin master --force
+
+  echo -e "Published Javadoc to gh-pages.\n"
+  )
+
 fi
