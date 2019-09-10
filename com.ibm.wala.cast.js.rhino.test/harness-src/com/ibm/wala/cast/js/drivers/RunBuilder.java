450d61b48efcb7a573294f0d0c24301ba05d51c6
Julian Dolby <juliandolby@gmail.com>
2019-09-10 09:24:22 -0400

    more checking

--- com.ibm.wala.cast.js.rhino.test/harness-src/com/ibm/wala/cast/js/drivers/RunBuilder.java
+++ com.ibm.wala.cast.js.rhino.test/harness-src/com/ibm/wala/cast/js/drivers/RunBuilder.java
@@ -11,9 +11,16 @@
 package com.ibm.wala.cast.js.drivers;
 
 import com.ibm.wala.cast.ipa.callgraph.CAstCallGraphUtil;
+import com.ibm.wala.cast.ir.translator.RewritingTranslatorToCAst;
+import com.ibm.wala.cast.ir.translator.TranslatorToCAst;
 import com.ibm.wala.cast.js.ipa.callgraph.JSCFABuilder;
 import com.ibm.wala.cast.js.translator.CAstRhinoTranslatorFactory;
+import com.ibm.wala.cast.js.translator.RhinoToAstTranslator;
 import com.ibm.wala.cast.js.util.JSCallGraphBuilderUtil;
+import com.ibm.wala.cast.js.util.JSCallGraphBuilderUtil.CGBuilderType;
+import com.ibm.wala.cast.tree.CAst;
+import com.ibm.wala.cast.tree.impl.CAstImpl;
+import com.ibm.wala.classLoader.ModuleEntry;
 import com.ibm.wala.ipa.callgraph.CallGraph;
 import com.ibm.wala.ipa.callgraph.propagation.CPAContextSelector;
 import com.ibm.wala.util.CancelException;
@@ -25,10 +32,27 @@ public class RunBuilder {
   public static void main(String[] args)
       throws IOException, WalaException, IllegalArgumentException, CancelException {
 
-    com.ibm.wala.cast.js.ipa.callgraph.JSCallGraphUtil.setTranslatorFactory(
-        new CAstRhinoTranslatorFactory());
+    class CAstRhinoNewTranslator extends RewritingTranslatorToCAst {
+      public CAstRhinoNewTranslator(ModuleEntry m, boolean replicateForDoLoops) {
+        super(
+            m, new RhinoToAstTranslator(new CAstImpl(), m, m.getName(), replicateForDoLoops, true));
+      }
+    }
 
-    JSCFABuilder builder = JSCallGraphBuilderUtil.makeScriptCGBuilder(args[0], args[1]);
+    com.ibm.wala.cast.js.ipa.callgraph.JSCallGraphUtil.setTranslatorFactory(
+        new CAstRhinoTranslatorFactory() {
+          @Override
+          public TranslatorToCAst make(CAst ast, ModuleEntry M) {
+            return new CAstRhinoNewTranslator(M, false);
+          }
+        });
+
+    JSCFABuilder builder =
+        JSCallGraphBuilderUtil.makeScriptCGBuilder(
+            args[0],
+            args[1],
+            CGBuilderType.ZERO_ONE_CFA_WITHOUT_CORRELATION_TRACKING,
+            RunBuilder.class.getClassLoader());
     builder.setContextSelector(new CPAContextSelector(builder.getContextSelector()));
     CallGraph CG = builder.makeCallGraph(builder.getOptions());
     CAstCallGraphUtil.AVOID_DUMP = false;
