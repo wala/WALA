e23125cb2abae2eac192a98767dd6ff37b5ef032
Ben Liblit <liblit@acm.org>
2020-01-19 19:23:53 -0500

    Suppress a benign IntelliJ IDEA JUnit-related warning

--- com.ibm.wala.core.tests/src/test/java/com/ibm/wala/core/tests/ir/TypeAnnotationTest.java
+++ com.ibm.wala.core.tests/src/test/java/com/ibm/wala/core/tests/ir/TypeAnnotationTest.java
@@ -45,6 +45,7 @@ import java.util.List;
 import java.util.Map;
 import org.junit.Test;
 
+@SuppressWarnings("UnconstructableJUnitTestCase")
 public class TypeAnnotationTest extends WalaTestCase {
 
   private final IClassHierarchy cha;
