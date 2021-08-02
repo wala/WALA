29b9926b1d2022aa3cdcae93923fdf1d3a52b199
Jerry <xuveda@gmail.com>
2021-08-02 11:58:01 -0700

    changed RetT to no longer println the input, just calls GetT.

--- com.ibm.wala.core/src/test11Subjects/java/privateInterfaceMethods/PrivateInterface.java
+++ com.ibm.wala.core/src/test11Subjects/java/privateInterfaceMethods/PrivateInterface.java
@@ -2,7 +2,7 @@ package privateInterfaceMethods;
 
 public interface PrivateInterface {//called RetT and GetT b/c it used to type T return/input
   default <Object> void RetT(Object input) {
-    System.out.println(GetT(input));
+    GetT(input);
   }
 
   private <Object> Object GetT(Object input) {
