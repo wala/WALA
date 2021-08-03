d9212447335c0c25793970cfcc8c06043559a5cd
Jerry <xuveda@gmail.com>
2021-08-03 10:21:04 -0700

    Method is no longer generic

--- com.ibm.wala.core/src/test11Subjects/java/privateInterfaceMethods/PrivateInterface.java
+++ com.ibm.wala.core/src/test11Subjects/java/privateInterfaceMethods/PrivateInterface.java
@@ -1,11 +1,11 @@
 package privateInterfaceMethods;
 
-public interface PrivateInterface {//called RetT and GetT b/c it used to type T return/input
-  default <Object> void RetT(Object input) {
+public interface PrivateInterface { // called RetT and GetT b/c it used to type T return/input
+  default void RetT(Object input) {
     GetT(input);
   }
 
-  private <Object> Object GetT(Object input) {
+  private Object GetT(Object input) {
     return input;
   }
 }
