bf980891eaf49db656df70dd7c73ac894258849d
Jerry <xuveda@gmail.com>
2021-08-21 16:15:34 -0700

    initial commit for testing string concat

---
+++ com.ibm.wala.core/src/test11Subjects/java/stringConcat/StringConcat.java
@@ -0,0 +1,10 @@
+package stringConcat;
+
+public class StringConcat {
+    String testConcat(){
+        return "foo" + "bar";
+    }
+    public void main(String[] args){
+        testConcat();
+    }
+}
