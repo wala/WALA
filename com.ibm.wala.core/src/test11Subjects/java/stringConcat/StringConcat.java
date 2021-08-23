3f716a13a51f1030527075ab242c2dd6c849cfd2
Jerry <xuveda@gmail.com>
2021-08-23 13:48:40 -0700

    initial commit for testing string concat

--- com.ibm.wala.core/src/test11Subjects/java/stringConcat/StringConcat.java
+++ com.ibm.wala.core/src/test11Subjects/java/stringConcat/StringConcat.java
@@ -2,7 +2,12 @@ package stringConcat;
 
 public class StringConcat {
   String testConcat() {
-    return "foo" + "bar";
+    String s1 = "thing 1";
+    String s2 = "thing 2";
+
+    String s3 = s1 + s2;
+
+    return s3 + "foobar";
   }
 
   public void main(String[] args) {
