e087c08744a2ebbd146dfc7dda09ccb6a8a6b7aa
Manu Sridharan <msridhar@gmail.com>
2021-09-14 17:22:01 -0700

    update copyright headers

--- com.ibm.wala.util/src/test/java/com/ibm/wala/util/test/BasicGraphTest.java
+++ com.ibm.wala.util/src/test/java/com/ibm/wala/util/test/BasicGraphTest.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2021 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Manu Sridharan - initial API and implementation
+ */
 package com.ibm.wala.util.test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
