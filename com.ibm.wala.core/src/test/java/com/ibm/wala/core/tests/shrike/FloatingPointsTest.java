699bae38d01dd154288feae39b05c9699c3a4866
Manu Sridharan <msridhar@gmail.com>
2021-11-26 15:12:15 -0800

    Fix FloatingPointsTest

--- com.ibm.wala.core/src/test/java/com/ibm/wala/core/tests/shrike/FloatingPointsTest.java
+++ com.ibm.wala.core/src/test/java/com/ibm/wala/core/tests/shrike/FloatingPointsTest.java
@@ -36,7 +36,7 @@ public class FloatingPointsTest extends WalaTestCase {
   }
 
   public FloatingPointsTest() {
-    this(getClasspathEntry("testSubjects"));
+    this(getClasspathEntry("classes" + File.separator + "java" + File.separator + "testSubjects"));
   }
 
   @Before
