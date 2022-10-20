0ff2fa871adee6f9ee814277936b6f8ecb7ac46f
nimakarimipour <karimipour.nima@gmail.com>
2022-10-20 01:57:30 -0700

    update script

--- annotator/runner.py
+++ annotator/runner.py
@@ -4,7 +4,7 @@ import subprocess
 from pathlib import Path
 
 MODULES = {
-  "com.ibm.wala.util":[
+  "com.ibm.wala.util": [
     "com.ibm.wala.cast.java.ecj",
     "com.ibm.wala.cast.java",
     "com.ibm.wala.cast.js.nodejs",
@@ -23,15 +23,22 @@ MODULES = {
 }
 args = []
 target = "com.ibm.wala.util"
-core = "~/.m2/repository/edu/ucr/cs/riple/nullawayannotator/core/1.3.4-SNAPSHOT"
+core = "{}/.m2/repository/edu/ucr/cs/riple/nullawayannotator/core/1.3.4-SNAPSHOT".format(
+  Path.home())
 downstream_enabled = True
 repo_path = Path(os.getcwd()).parent.absolute()
-build_command = "cd {} && ANNOTATOR_TARGET={} ./gradlew {}:compileJava --rerun-tasks".format(repo_path, target, target)
-downstream_build_command = "cd {} && ANNOTATOR_TARGET={} ./gradlew {} --rerun-tasks".format(repo_path, target, ' '.join(["{}:compileJava".format(dep) for dep in MODULES[target]]))
-nullaway_library_model_loader = "{}/com.ibm.wala.librarymodelsloader/src/main/resources/com/ibm/wala/librarymodelsloader/nullable-methods.tsv".format(repo_path)
+build_command = "cd {} && ANNOTATOR_TARGET={} ./gradlew {}:compileJava --rerun-tasks".format(
+  repo_path, target, target)
+downstream_build_command = "cd {} && ANNOTATOR_TARGET={} ./gradlew {} --rerun-tasks".format(
+  repo_path, target,
+  ' '.join(["{}:compileJava".format(dep) for dep in MODULES[target]]))
+nullaway_library_model_loader = "{}/com.ibm.wala.librarymodelsloader/src/main/resources/com/ibm/wala/librarymodelsloader/nullable-methods.tsv".format(
+  repo_path)
 out_dir = "/tmp/NullAwayFix"
-config_paths = ["{}\t{}\n".format("{}/{}/config/nullaway.xml".format(repo_path, dep), "{}/{}/config/scanner.xml".format(repo_path, dep)) for dep in MODULES[target]]
-print(config_paths)
+config_paths = [
+  "{}\t{}\n".format("{}/{}/config/nullaway.xml".format(repo_path, dep),
+                    "{}/{}/config/scanner.xml".format(repo_path, dep)) for dep
+  in MODULES[target]]
 paths = "/tmp/NullAwayFix/paths.tsv"
 f = open(paths, "w")
 f.writelines(config_paths)
@@ -48,6 +55,5 @@ if downstream_enabled:
   args += ["-ddbc", downstream_build_command]
 
 print(args)
-
-subprocess.Popen(["cd", core] + ["java", "-jar", "core-1.3.4-SNAPSHOT.jar"] + args)
-
+os.chdir(core)
+subprocess.Popen(["java", "-jar", "core-1.3.4-SNAPSHOT.jar"] + args)
