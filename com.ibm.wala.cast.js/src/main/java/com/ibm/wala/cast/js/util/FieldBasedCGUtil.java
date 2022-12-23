1ff7c5c621d0fa7168db3042596ef247dc3fe130
Ben Liblit <liblit@acm.org>
2022-12-23 16:03:24 -0500

    Avoid resource leaks when walking the file system

--- com.ibm.wala.cast.js/src/main/java/com/ibm/wala/cast/js/util/FieldBasedCGUtil.java
+++ com.ibm.wala.cast.js/src/main/java/com/ibm/wala/cast/js/util/FieldBasedCGUtil.java
@@ -168,10 +168,11 @@ public class FieldBasedCGUtil {
   }
 
   public List<Module> findScriptsInDir(Path scriptDir) throws IOException {
-    List<Path> jsFiles =
-        Files.walk(scriptDir)
-            .filter(p -> p.toString().toLowerCase().endsWith(".js"))
-            .collect(Collectors.toList());
+    final List<Path> jsFiles;
+    try (var walk = Files.walk(scriptDir)) {
+      jsFiles =
+          walk.filter(p -> p.toString().toLowerCase().endsWith(".js")).collect(Collectors.toList());
+    }
     List<Module> scripts = new ArrayList<>();
     // we can't do this loop as a map() operation on the previous stream because toURL() throws
     // a checked exception
