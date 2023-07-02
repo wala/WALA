a4652da15409c486e9e40875ff56303700e2d0d0
Ben Liblit <liblit@acm.org>
2023-07-01 22:36:50 -0400

    Simplify Maven Eclipse configuration
    A new `walaEclipseMavenCentral` Kotlin DSL extension allows configuring
    the diffplug MavenCentral plugin more easily by internalizing certain
    WALA-specific behaviors and adding a few convenience features.  See KDoc
    comments in
    `build-logic/src/main/kotlin/com/ibm/wala/gradle/eclipse-maven-central.gradle.kts`
    for more details about what this new extension can do.
    
    Resolves wala/WALA#1280.

--- ide/jdt/build.gradle.kts
+++ ide/jdt/build.gradle.kts
@@ -1,25 +1,20 @@
 plugins {
-  id("com.diffplug.eclipse.mavencentral")
+  id("com.ibm.wala.gradle.eclipse-maven-central")
   id("com.ibm.wala.gradle.java")
 }
 
-eclipseMavenCentral {
-  release(rootProject.extra["eclipseVersion"] as String) {
-    api("org.eclipse.equinox.common")
-    listOf(
-            "org.eclipse.core.jobs",
-            "org.eclipse.core.resources",
-            "org.eclipse.core.runtime",
-            "org.eclipse.equinox.app",
-            "org.eclipse.jdt.core",
-            "org.eclipse.jface",
-            "org.eclipse.osgi",
-            "org.eclipse.ui.workbench",
-        )
-        .forEach { implementation(it) }
-    useNativesForRunningPlatform()
-    constrainTransitivesToThisRelease()
-  }
+walaEclipseMavenCentral {
+  api("org.eclipse.equinox.common")
+  implementation(
+      "org.eclipse.core.jobs",
+      "org.eclipse.core.resources",
+      "org.eclipse.core.runtime",
+      "org.eclipse.equinox.app",
+      "org.eclipse.jdt.core",
+      "org.eclipse.jface",
+      "org.eclipse.osgi",
+      "org.eclipse.ui.workbench",
+  )
 }
 
 dependencies {
