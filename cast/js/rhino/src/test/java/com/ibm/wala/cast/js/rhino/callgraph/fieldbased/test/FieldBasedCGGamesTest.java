775557f17896b3823d917f0205d08b1e92576d1b
Manu Sridharan <msridhar@gmail.com>
2023-12-17 15:28:20 -0800

    remove deprecated calls

--- cast/js/rhino/src/test/java/com/ibm/wala/cast/js/rhino/callgraph/fieldbased/test/FieldBasedCGGamesTest.java
+++ cast/js/rhino/src/test/java/com/ibm/wala/cast/js/rhino/callgraph/fieldbased/test/FieldBasedCGGamesTest.java
@@ -5,6 +5,9 @@ import com.ibm.wala.cast.js.util.FieldBasedCGUtil.BuilderType;
 import com.ibm.wala.util.CancelException;
 import com.ibm.wala.util.WalaException;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Tag;
@@ -15,21 +18,29 @@ public class FieldBasedCGGamesTest extends AbstractFieldBasedTest {
 
   @Test
   public void testBunnyHunt() throws IOException, WalaException, Error, CancelException {
-    URL url = new URL("http://www.themaninblue.com/experiment/BunnyHunt/");
+    URL url = getUrl("http://www.themaninblue.com/experiment/BunnyHunt/");
     runTest(url, new Object[][] {}, BuilderType.OPTIMISTIC);
+  }
+
+  private static URL getUrl(String urlStr) throws MalformedURLException {
+    try {
+      return new URI(urlStr).toURL();
+    } catch (URISyntaxException e) {
+      throw new RuntimeException(e);
+    }
   }
 
   @Disabled("seems to break with http issues")
   @Test
   public void testBeslimed() throws IOException, WalaException, Error, CancelException {
-    URL url = new URL("http://www.markus-inger.de/test/game.php");
+    URL url = getUrl("http://www.markus-inger.de/test/game.php");
     runTest(url, new Object[][] {}, BuilderType.OPTIMISTIC);
   }
 
   @Disabled("seems to break with http issues")
   @Test
   public void testDiggAttack() throws IOException, WalaException, Error, CancelException {
-    URL url = new URL("http://www.pixastic.com/labs/digg_attack/");
+    URL url = getUrl("http://www.pixastic.com/labs/digg_attack/");
     runTest(url, new Object[][] {}, BuilderType.OPTIMISTIC);
   }
 
@@ -37,7 +48,7 @@ public class FieldBasedCGGamesTest extends AbstractFieldBasedTest {
   @Test
   public void testRiverRaider() throws IOException, WalaException, Error, CancelException {
     URL url =
-        new URL(
+        getUrl(
             "http://playstar.mobi/games/riverraider/index.html?playerId=&gameId=8&highscore=102425");
     runTest(url, new Object[][] {}, BuilderType.OPTIMISTIC_WORKLIST);
   }
@@ -45,31 +56,31 @@ public class FieldBasedCGGamesTest extends AbstractFieldBasedTest {
   @Disabled("fails with \"timed out\" CancelException")
   @Test
   public void testSolitaire() throws IOException, WalaException, Error, CancelException {
-    URL url = new URL("http://www.inmensia.com/files/solitaire1.0.html");
+    URL url = getUrl("http://www.inmensia.com/files/solitaire1.0.html");
     runTest(url, new Object[][] {}, BuilderType.OPTIMISTIC);
   }
 
   @Test // (expected = CancelException.class)
   public void testWorldOfSolitaire() throws IOException, WalaException, Error, CancelException {
-    URL url = new URL("http://worldofsolitaire.com/");
+    URL url = getUrl("http://worldofsolitaire.com/");
     runTest(url, new Object[][] {}, BuilderType.OPTIMISTIC);
   }
 
   @Test
   public void testMinesweeper() throws IOException, WalaException, Error, CancelException {
-    URL url = new URL("http://www.inmensia.com/files/minesweeper1.0.html");
+    URL url = getUrl("http://www.inmensia.com/files/minesweeper1.0.html");
     runTest(url, new Object[][] {}, BuilderType.OPTIMISTIC_WORKLIST);
   }
 
   @Test
   public void testProtoRPG() throws IOException, WalaException, Error, CancelException {
-    URL url = new URL("http://www.protorpg.com/games/protorpg/?game=prologue");
+    URL url = getUrl("http://www.protorpg.com/games/protorpg/?game=prologue");
     runTest(url, new Object[][] {}, BuilderType.OPTIMISTIC_WORKLIST);
   }
 
   @Test
   public void testBattleship() throws IOException, WalaException, Error, CancelException {
-    URL url = new URL("http://www.sinkmyship.com/battleship/single.html");
+    URL url = getUrl("http://www.sinkmyship.com/battleship/single.html");
     runTest(url, new Object[][] {}, BuilderType.OPTIMISTIC_WORKLIST);
   }
 }
