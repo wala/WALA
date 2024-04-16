d09462c704439ec8a7be6fdb7363c128ffbef534
Aakash Gnanakumar <aakashappiah@gmail.com>
2024-04-15 19:56:46 -0700

    added comments on toJson

--- core/src/main/java/com/ibm/wala/ipa/cha/ClassHierarchy.java
+++ core/src/main/java/com/ibm/wala/ipa/cha/ClassHierarchy.java
@@ -707,22 +707,28 @@ public class ClassHierarchy implements IClassHierarchy {
     }
   }
 
+  // Puts ClassHierarchy into a JSON readable variable that maps the class to a list of its subclasses
   public Object toJson() {
-    HashMap<Object, Object> serial = new HashMap<>();
+    // lines 713-715: initialize variables (subclass and dag) to store important values like hashmap of each iteration and a list of all maps of class to subclasses
+    HashMap<Object, Object> subclass = new HashMap<>();
     Iterator<Node> children = root.getChildren();
     ArrayList<HashMap<Object, Object>> dag = new ArrayList<>();
+    // lines 717-721: Goes through all subclasses of the root class, and adds the subclass hashmap to the list of all subclass hashmaps
     while (children.hasNext()) {
       Node temp = children.next();
-      HashMap<Object, Object> fin = helper_toJson(temp, serial);
+      HashMap<Object, Object> fin = helper_toJson(temp, subclass);
       dag.add(fin);
     }
+    // lines 723-725: Puts all hashmaps into one larger hashmap
     for (int i = 1; i < dag.size(); i++) {
       dag.get(0).putAll(dag.get(i));
     }
+    // lines 727-730: Removes unnecesarry parts from name of the class
     String key = root.getJavaClass().toString();
     key = key.replace("<Primordial,", "");
     key = key.replace("<Application,", "");
     key = key.replace(">", "");
+    // lines 732-742: inserting the root class to its subclasses into the main hashmap while removing unnecessary substrings from the class name
     Iterator<Node> root_children = root.getChildren();
     Set<String> final_root_children = new HashSet<>();
     while(root_children.hasNext()) {
@@ -736,18 +742,24 @@ public class ClassHierarchy implements IClassHierarchy {
     dag.get(0).put(key, final_root_children);
     return dag.get(0);
   }
-
+  // helper function to toJson that performs recursion to go through all of the DAG
   public HashMap<Object, Object> helper_toJson(Node n, HashMap<Object, Object> hash) {
+    // Base case of no subclasses allowing recursion to stop
     if (n.children.size() > 0) {
+      // line 750 + while loop: defines iterator to go through the class's subclasses
       Iterator<Node> children = n.getChildren();
       while (children.hasNext()) {
+        // line 753-754: puts subclass variable into temp, and start recursion from that subclass until base case reaches
         Node temp = children.next();
-        HashMap<Object, Object> temp_hash = helper_toJson(temp, hash);
+        helper_toJson(temp, hash);
+        // line 756-759: remove unnecessary substrings from class name
         String key = temp.getJavaClass().toString();
         key = key.replace("<Primordial,", "");
         key = key.replace("<Application,", "");
         key = key.replace(">", "");
+        // if statement: only adds the map from class to subclass if class name does not exist in the overall hashmap
         if (!hash.containsKey(key)) {
+          //lines 763-773: Iterates through the subclass list and removes unnecessary substrings from class name --> adds that new value into varibale, root, containig all it's subclasses
           Iterator<Node> val = temp.getChildren();
           Set<String> root = new HashSet<>();
           while(val.hasNext()) {
@@ -758,11 +770,12 @@ public class ClassHierarchy implements IClassHierarchy {
             new_val = new_val.replace(">", "");
             root.add(new_val);
           }
+          // line 774: insert into hashmap containing all class to subclass pairs
           hash.put(key, root);
         }
-        System.out.println(temp_hash);
       }
     } else {
+      // line 780-787: remove unnecessary substring from class name and map that to an empty list to add into the hashmap
       String key = n.getJavaClass().toString();
       key = key.replace("<Primordial,", "");
       key = key.replace("<Application,", "");
