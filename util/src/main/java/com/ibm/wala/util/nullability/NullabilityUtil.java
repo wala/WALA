a941891309c38bb5179565612ff1b6a32bcb1d8e
Manu Sridharan <msridhar@gmail.com>
2024-07-30 11:14:45 -0700

    javadoc

--- util/src/main/java/com/ibm/wala/util/nullability/NullabilityUtil.java
+++ util/src/main/java/com/ibm/wala/util/nullability/NullabilityUtil.java
@@ -2,19 +2,34 @@ package com.ibm.wala.util.nullability;
 
 import org.jspecify.annotations.Nullable;
 
+/** Utility methods for targeted suppressions of NullAway warnings. */
 public class NullabilityUtil {
 
+  /** Exception thrown when a {@link #castToNonNull(Object)} call fails. */
   public static class CastToNonNullFailedException extends RuntimeException {
     private static final long serialVersionUID = 1L;
   }
 
-  public static <T> T castToNonNull(@Nullable T obj) {
+  /**
+   * Casts a reference to a {@code @NonNull} type, throwing an exception if the reference is null.
+   *
+   * @param obj the reference
+   * @return the reference itself
+   * @throws CastToNonNullFailedException if {@code obj} is null
+   */
+  public static <T> T castToNonNull(@Nullable T obj) throws CastToNonNullFailedException {
     if (obj == null) {
       throw new CastToNonNullFailedException();
     }
     return obj;
   }
 
+  /**
+   * Returns a null reference with a {@code @NonNull} type. Useful, e.g., where a method is allowed
+   * to return null, but the NullAway type system can only express a {@code @NonNull} return type.
+   *
+   * @return a null reference
+   */
   @SuppressWarnings("NullAway")
   public static <T extends @Nullable Object> T uncheckedNull() {
     return null;
