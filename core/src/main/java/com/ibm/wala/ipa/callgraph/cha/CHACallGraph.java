bf4bbc9e86086e2ad9647c2d0d2862aceceae10e
Manu Sridharan <msridhar@gmail.com>
2024-11-26 12:02:43 -0800

    Don't update CHACallGraph internal cache in methods that read its state

--- core/src/main/java/com/ibm/wala/ipa/callgraph/cha/CHACallGraph.java
+++ core/src/main/java/com/ibm/wala/ipa/callgraph/cha/CHACallGraph.java
@@ -148,7 +148,13 @@ public class CHACallGraph extends BasicCallGraph<CHAContextInterpreter> {
 
   private final Map<CallSiteReference, Set<IMethod>> targetCache = HashMapFactory.make();
 
-  private Iterator<IMethod> getPossibleTargets(CallSiteReference site) {
+  /**
+   * Gets the possible targets of a call site, caching the result if it has not been computed.
+   *
+   * @param site the call site
+   * @return an iterator of possible targets
+   */
+  private Iterator<IMethod> getOrUpdatePossibleTargets(CallSiteReference site) {
     Set<IMethod> result = targetCache.get(site);
     if (result == null) {
       if (site.isDispatch()) {
@@ -166,11 +172,25 @@ public class CHACallGraph extends BasicCallGraph<CHAContextInterpreter> {
     return result.iterator();
   }
 
+  /**
+   * Gets the possible targets of a call site from the cache.
+   *
+   * @param site the call site
+   * @return an iterator of possible targets
+   */
+  private Iterator<IMethod> getPossibleTargetsFromCache(CallSiteReference site) {
+    Set<IMethod> result = targetCache.get(site);
+    if (result == null) {
+      return Collections.emptyIterator();
+    }
+    return result.iterator();
+  }
+
   @Override
   public Set<CGNode> getPossibleTargets(CGNode node, CallSiteReference site) {
     return Iterator2Collection.toSet(
         new MapIterator<>(
-            new FilterIterator<>(getPossibleTargets(site), this::isRelevantMethod),
+            new FilterIterator<>(getPossibleTargetsFromCache(site), this::isRelevantMethod),
             object -> {
               try {
                 return findOrCreateNode(object, Everywhere.EVERYWHERE);
@@ -183,7 +203,7 @@ public class CHACallGraph extends BasicCallGraph<CHAContextInterpreter> {
 
   @Override
   public int getNumberOfTargets(CGNode node, CallSiteReference site) {
-    return IteratorUtil.count(getPossibleTargets(site));
+    return IteratorUtil.count(getPossibleTargetsFromCache(site));
   }
 
   @Override
@@ -259,7 +279,7 @@ public class CHACallGraph extends BasicCallGraph<CHAContextInterpreter> {
     while (!newNodes.isEmpty()) {
       CGNode n = newNodes.pop();
       for (CallSiteReference site : Iterator2Iterable.make(n.iterateCallSites())) {
-        Iterator<IMethod> methods = getPossibleTargets(site);
+        Iterator<IMethod> methods = getOrUpdatePossibleTargets(site);
         while (methods.hasNext()) {
           IMethod target = methods.next();
           if (isRelevantMethod(target)) {
