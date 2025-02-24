8e72ff69a09f0d808a7914cfbe067291901dcd5e
LisaHLi <lisa.h.li@ibm.com>
2025-02-24 11:26:02 -0500

    Undo the change

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,7 +51,7 @@ JavaVersion.current().let {
 
 group = name
 
-version = "1.1.0-rc1"
+version = "1.6.8-SNAPSHOT"
 
 // version of Eclipse JARs to use for Eclipse-integrated WALA components.
 val eclipseVersion: EclipseRelease by extra {
