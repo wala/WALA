e8ed6017454857e1d0c44ab1757ae48a027f4414
Jacob Khaliqi <jacobkhaliqi@gmail.com>
2025-03-13 11:21:39 -0700

    used runTest wrapper for testComments

--- cast/java/ecj/src/test/java/com/ibm/wala/cast/java/test/ECJTestComments.java
+++ cast/java/ecj/src/test/java/com/ibm/wala/cast/java/test/ECJTestComments.java
@@ -59,14 +59,7 @@ public class ECJTestComments extends IRTests {
 
   @Test
   public void testComments() throws IllegalArgumentException, CancelException, IOException {
-    Pair<CallGraph, CallGraphBuilder<? super InstanceKey>> result =
-        runTest(
-            singleTestSrc("Comments"),
-            rtJar,
-            simpleTestEntryPoint("Comments"),
-            emptyList,
-            true,
-            null);
+    Pair<CallGraph, CallGraphBuilder<? super InstanceKey>> result = runTest("Comments");
     for (CGNode node : result.fst.getNodes(testMethod)) {
       if (node.getMethod() instanceof AstMethod) {
         AstMethod m = (AstMethod) node.getMethod();
