01e4d98eed53a608b63fba924c0e41a2cb0ce7b5
Manu Sridharan <msridhar@gmail.com>
2025-04-11 13:46:44 -0700

    Fix comment

--- shrike/src/main/java/com/ibm/wala/shrike/shrikeCT/ClassReader.java
+++ shrike/src/main/java/com/ibm/wala/shrike/shrikeCT/ClassReader.java
@@ -67,7 +67,7 @@ public final class ClassReader implements ClassConstants {
     if (magic != MAGIC) {
       throw new InvalidClassFileException(offset, "bad magic number: " + magic);
     }
-    // Support class files up through JDK 23 (version 67)
+    // Support class files up through JDK 24 (version 68)
     if (majorVersion < 45 || majorVersion > 68) {
       throw new InvalidClassFileException(
           offset, "unknown class file version: " + majorVersion + '.' + minorVersion);
