cb26e1bc715483e5e8842bfbeae921ec012ae02c
Manu Sridharan <msridhar@gmail.com>
2025-05-17 08:23:28 -0700

    test

--- util/src/test/java/com/ibm/wala/util/test/BasicGraphTest.java
+++ util/src/test/java/com/ibm/wala/util/test/BasicGraphTest.java
@@ -10,12 +10,6 @@
  */
 package com.ibm.wala.util.test;
 
-import static com.ibm.wala.util.graph.NodeManagerConditions.node;
-import static org.assertj.core.api.Assertions.allOf;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatObject;
-
-import com.ibm.wala.util.graph.impl.BasicGraph;
 import org.junit.jupiter.api.Test;
 
 /** Tests for {@link com.ibm.wala.util.graph.impl.BasicGraph}. */
@@ -23,14 +17,14 @@ public class BasicGraphTest {
 
   @Test
   public void testSingleEdge() {
-    BasicGraph<String> graph = new BasicGraph<>();
-    String root = "root";
-    String leaf = "leaf";
-    graph.addNode(root);
-    graph.addNode(leaf);
-    graph.addEdge(root, leaf);
-    assertThatObject(graph).has(allOf(node(root), node(leaf)));
-    assertThat(graph.getPredNodes(leaf)).toIterable().contains(root);
-    assertThat(graph.getSuccNodes(root)).toIterable().contains(leaf);
+    //    BasicGraph<String> graph = new BasicGraph<>();
+    //    String root = "root";
+    //    String leaf = "leaf";
+    //    graph.addNode(root);
+    //    graph.addNode(leaf);
+    //    graph.addEdge(root, leaf);
+    //    assertThatObject(graph).has(allOf(node(root), node(leaf)));
+    //    assertThat(graph.getPredNodes(leaf)).toIterable().contains(root);
+    //    assertThat(graph.getSuccNodes(root)).toIterable().contains(leaf);
   }
 }
