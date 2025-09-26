236b3e94079c81633f5b989ebb6c61e6018ec0e9
Ben Liblit <liblit@acm.org>
2025-09-26 13:43:30 -0400

    Simplify some Kotlin functions
    A Kotlin function with a single-statement block body can use an
    expression body instead.
    
    A Kotlin function that explicitly `null`-checks a platform type can use
    an explicit return type instead.

--- build-logic/src/main/kotlin/com/ibm/wala/gradle/Exec-useCurrentJavaHome.kt
+++ build-logic/src/main/kotlin/com/ibm/wala/gradle/Exec-useCurrentJavaHome.kt
@@ -3,6 +3,5 @@ package com.ibm.wala.gradle
 import org.gradle.api.tasks.Exec
 
 /** Configures this [Exec] task to run with `JAVA_HOME` set to the current Gradle JVM's home. */
-fun Exec.useCurrentJavaHome() {
-  environment("JAVA_HOME", project.providers.systemProperty("java.home").valueToString)
-}
+fun Exec.useCurrentJavaHome() =
+    environment("JAVA_HOME", project.providers.systemProperty("java.home").valueToString)
