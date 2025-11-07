ce61817fbbafcf5a9a54007051a1dc249dec806b
Ben Liblit <liblit@acm.org>
2025-11-07 15:54:00 -0500

    Update AssertJ (#1753)

--- util/src/testFixtures/java/com/ibm/wala/util/intset/IntSetAssert.java
+++ util/src/testFixtures/java/com/ibm/wala/util/intset/IntSetAssert.java
@@ -3,11 +3,11 @@ package com.ibm.wala.util.intset;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import org.assertj.core.annotation.CheckReturnValue;
 import org.assertj.core.api.AbstractCollectionAssert;
 import org.assertj.core.api.AbstractObjectAssert;
 import org.assertj.core.api.Assertions;
 import org.assertj.core.api.ObjectAssert;
-import org.assertj.core.util.CheckReturnValue;
 
 public class IntSetAssert extends AbstractObjectAssert<IntSetAssert, IntSet> {
 
