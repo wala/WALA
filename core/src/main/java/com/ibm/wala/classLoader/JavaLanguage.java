9b858a38440b0f0da93c8005d5ebbcfb276c4f7a
Julian <julian@certora.com>
2026-04-02 12:59:22 -0400

    make nested arrays as objects into a query on Language

--- core/src/main/java/com/ibm/wala/classLoader/JavaLanguage.java
+++ core/src/main/java/com/ibm/wala/classLoader/JavaLanguage.java
@@ -858,4 +858,9 @@ public class JavaLanguage extends LanguageImpl implements BytecodeLanguage, Cons
       boolean ignoreAllocHeapDefs) {
     return new ModVisitor<>(n, result, h, pa, ignoreAllocHeapDefs);
   }
+  
+  @Override
+  public boolean arraysAsNestedObject() {
+    return true;
+  }
 }
--- core/src/main/java/com/ibm/wala/classLoader/Language.java
+++ core/src/main/java/com/ibm/wala/classLoader/Language.java
@@ -149,4 +149,9 @@ public interface Language {
       PointerAnalysis<T> pa,
       ExtendedHeapModel h,
       boolean ignoreAllocHeapDefs);
+
+  /** whether language has arrays as nested object types, i.e. A[]. A[][], etc. */
+  default boolean arraysAsNestedObject() {
+    return false;
+  }
 }
