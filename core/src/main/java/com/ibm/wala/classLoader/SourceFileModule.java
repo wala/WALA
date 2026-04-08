c4877be90582dea2068a60911975842e7a5e007b
Julian <julian@certora.com>
2026-04-08 17:40:46 -0400

    explain logical pathname

--- core/src/main/java/com/ibm/wala/classLoader/SourceFileModule.java
+++ core/src/main/java/com/ibm/wala/classLoader/SourceFileModule.java
@@ -60,6 +60,12 @@ public class SourceFileModule extends FileModule implements Module, ModuleEntry,
     return new InputStreamReader(getInputStream());
   }
 
+  /**
+   * Sometimes a file has a name that is not really a pathname, such as a name with respect to an
+   * include path specified elsewhere. We call this a logical name.
+   *
+   * @return a logical file name
+   */
   public String logicalFileName() {
     return fileName;
   }
