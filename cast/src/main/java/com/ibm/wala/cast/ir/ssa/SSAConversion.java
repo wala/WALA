335c33c15804f22800be6b3e4ce76a28d1493e0f
Julian <julian@certora.com>
2026-04-08 20:21:35 -0400

    remove a hack

--- cast/src/main/java/com/ibm/wala/cast/ir/ssa/SSAConversion.java
+++ cast/src/main/java/com/ibm/wala/cast/ir/ssa/SSAConversion.java
@@ -322,10 +322,6 @@ public class SSAConversion extends AbstractSSAConversion {
     @Override
     public String[] getLocalNames(int pc, int vn) {
 
-      if (computedNames.length <= vn) {
-        return null;
-      }
-
       if (computedNames[vn] != null) {
         return computedNames[vn];
       }
