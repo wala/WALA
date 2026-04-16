1d7e28fef3600ec67666b90cb102bf0ea7bd1ebc
Manu Sridharan <msridhar@gmail.com>
2026-04-16 15:14:30 -0700

    spotless

--- util/src/main/java/com/ibm/wala/util/PlatformUtil.java
+++ util/src/main/java/com/ibm/wala/util/PlatformUtil.java
@@ -22,7 +22,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import org.jspecify.annotations.Nullable;
 
 /** Platform-specific utility functions. */
 public class PlatformUtil {
