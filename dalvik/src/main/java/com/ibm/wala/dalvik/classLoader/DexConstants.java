180c0a92710de1191a5de0e2e8b37ef2866ff6d8
Ben Liblit <liblit@acm.org>
2026-04-19 16:25:40 -0400

    Initialize `DexConstants` from `ValueType`
    Many fields defined in `DexConstants` and `ValueType` have identical
    names and values.  The latter is narrower in scope, though, and is
    ultimately the "upstream" library upon which we depend.  So let's treat
    `ValueType` as canonical for the subset of things that it does
    cover.

--- dalvik/src/main/java/com/ibm/wala/dalvik/classLoader/DexConstants.java
+++ dalvik/src/main/java/com/ibm/wala/dalvik/classLoader/DexConstants.java
@@ -48,6 +48,7 @@
 package com.ibm.wala.dalvik.classLoader;
 
 import org.jf.dexlib2.AccessFlags;
+import org.jf.dexlib2.ValueType;
 
 public interface DexConstants {
   int ACC_PUBLIC = AccessFlags.PUBLIC.getValue();
@@ -78,20 +79,20 @@ public interface DexConstants {
   @Deprecated(forRemoval = true, since = "1.7.1")
   int ACC_private = ACC_PUBLIC;
 
-  int VALUE_BYTE = 0x00;
-  int VALUE_SHORT = 0x02;
-  int VALUE_CHAR = 0x03;
-  int VALUE_INT = 0x04;
-  int VALUE_LONG = 0x06;
-  int VALUE_FLOAT = 0x10;
-  int VALUE_DOUBLE = 0x11;
-  int VALUE_STRING = 0x17;
-  int VALUE_TYPE = 0x18;
-  int VALUE_FIELD = 0x19;
-  int VALUE_METHOD = 0x1a;
-  int VALUE_ENUM = 0x1b;
-  int VALUE_ARRAY = 0x1c;
-  int VALUE_ANNOTATION = 0x1d;
-  int VALUE_NULL = 0x1e;
-  int VALUE_BOOLEAN = 0x1f;
+  int VALUE_BYTE = ValueType.BYTE;
+  int VALUE_SHORT = ValueType.SHORT;
+  int VALUE_CHAR = ValueType.CHAR;
+  int VALUE_INT = ValueType.INT;
+  int VALUE_LONG = ValueType.LONG;
+  int VALUE_FLOAT = ValueType.FLOAT;
+  int VALUE_DOUBLE = ValueType.DOUBLE;
+  int VALUE_STRING = ValueType.STRING;
+  int VALUE_TYPE = ValueType.TYPE;
+  int VALUE_FIELD = ValueType.FIELD;
+  int VALUE_METHOD = ValueType.METHOD;
+  int VALUE_ENUM = ValueType.ENUM;
+  int VALUE_ARRAY = ValueType.ARRAY;
+  int VALUE_ANNOTATION = ValueType.ANNOTATION;
+  int VALUE_NULL = ValueType.NULL;
+  int VALUE_BOOLEAN = ValueType.BOOLEAN;
 }
