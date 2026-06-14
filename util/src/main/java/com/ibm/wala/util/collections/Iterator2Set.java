237d5bcc02da18b3592a1228dfb4f2411ae00aab
Ben Liblit <liblit@acm.org>
2026-06-14 17:13:57 -0400

    Deprecate `Iterator2List` and `Iterator2Set`
    We retain the static `Iterator2Collection.toList` and
    `Iterator2Collection.toSet` methods.  However, these now make weaker
    promises as to the types they return.  Internally, these are now
    implemented using existing Guava utility methods.  The collections that
    result are slightly more efficient than before, as they no longer route
    all `Collection` methods through an unnecessary proxy object.
    
    That being said, the underlying `Collection` types are the same as
    before: `toList` continues to be based on `ArrayList`, and `toSet`
    continues to be based on `LinkedHashSet`.  But now we get there using
    less of our own code and fewer delegation layers.
    
    Note that Guava is now an implementation dependency of our `utils`
    component.  Other parts of WALA were already using Guava, so this change
    should be easy for third-party code to handle.

--- util/src/main/java/com/ibm/wala/util/collections/Iterator2Set.java
+++ util/src/main/java/com/ibm/wala/util/collections/Iterator2Set.java
@@ -16,17 +16,20 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.Set;
 
+@Deprecated(forRemoval = true, since = "1.8.0")
 public class Iterator2Set<T> extends Iterator2Collection<T> implements Serializable, Set<T> {
 
   @Serial private static final long serialVersionUID = 3771468677527694694L;
 
   private final Set<T> delegate;
 
+  @Deprecated(forRemoval = true, since = "1.8.0")
   protected Iterator2Set(Iterator<? extends T> i, Set<T> delegate) {
     this.delegate = delegate;
     i.forEachRemaining(delegate::add);
   }
 
+  @Deprecated(forRemoval = true, since = "1.8.0")
   @Override
   protected Collection<T> getDelegate() {
     return delegate;
